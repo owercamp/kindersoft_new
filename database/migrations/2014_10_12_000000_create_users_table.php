71ded5c1b32104162ba0bba834d9ebd112aaf00b
Ower A Campos <owerion22@gmail.com>
2023-12-17 21:56:51 -0500

    refactor: se modifica la migración del usuario

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -14,7 +14,7 @@ return new class extends Migration
         Schema::create('users', function (Blueprint $table) {
             $table->id();
             $table->string('name');
-            $table->integer('document')->unique();
+            $table->integer('identification')->unique()->autoIncrement(false);
             $table->string('email')->unique();
             $table->timestamp('email_verified_at')->nullable();
             $table->string('password');
