c7bd6c979473bc85b33889e36947566fa69025d3
Ower A Campos <owerion22@gmail.com>
2023-12-21 12:19:04 -0500

    feat: dropdown de primer nivel

---
+++ resources/views/components/dropdown-first-level.blade.php
@@ -0,0 +1,8 @@
+<div x-data="{ open: false }" class="inline-flex items-center px-1 pt-1 border-b-2 border-transparent text-sm font-medium leading-5 text-gray-500 dark:text-gray-400 hover:text-gray-700 dark:hover:text-gray-300 hover:border-gray-300 dark:hover:border-gray-700 focus:outline-none focus:text-gray-700 dark:focus:text-gray-300 focus:border-gray-300 dark:focus:border-gray-700 transition duration-150 ease-in-out relative">
+  <button @click="open = !open" class="flex items-center justify-between w-full">
+    {{ $trigger }}
+  </button>
+  <div x-show="open" @click.away="open = false" class="absolute top-[4.1rem] left-0 min-w-[10rem] py-3 px-3 bg-slate-700 rounded-md flex flex-col gap-2">
+    {{ $content }}
+  </div>
+</div>
