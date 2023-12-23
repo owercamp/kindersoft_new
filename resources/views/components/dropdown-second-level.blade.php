99c3665ddc7d0ebf8c451fae41155c346d0143f3
Ower A Campos <owerion22@gmail.com>
2023-12-23 16:00:10 -0500

    refactor: se modifica la altura la lista

--- resources/views/components/dropdown-second-level.blade.php
+++ resources/views/components/dropdown-second-level.blade.php
@@ -1,6 +1,6 @@
 <div x-data="{ subMenuOpen1: false }" class="relative text-gray-500 dark:text-gray-400 hover:text-gray-700 dark:hover:text-gray-300 hover:border-gray-300 dark:hover:border-gray-700 focus:outline-none focus:text-gray-700 dark:focus:text-gray-300 focus:border-gray-300 dark:focus:border-gray-700 transition duration-150 ease-in-out flex">
   <button @click="subMenuOpen1 = !subMenuOpen1" class="flex items-center justify-between w-full">
-    <div class="w-full h-8 flex justify-center items-center text-center">
+    <div class="w-full h-9 flex justify-center items-center text-center">
       {{ $trigger }}
     </div>
     <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="currentColor" data-slot="icon" class="w-4 h-full">
