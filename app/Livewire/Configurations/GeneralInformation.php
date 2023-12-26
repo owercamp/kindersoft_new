21e410061d05b0a451aebf731f59df326454a360
Ower A Campos <owerion22@gmail.com>
2023-12-26 11:35:03 -0500

    feat: se crea el componente de clase general informations

--- app/Livewire/Configurations/GeneralInformation.php
+++ app/Livewire/Configurations/GeneralInformation.php
@@ -7,14 +7,8 @@ use Livewire\Component;
 class GeneralInformation extends Component
 {
 
-  public $numberContainers = 0;
-  public $nombre = "ower";
   public function render()
   {
     return view('livewire.configurations.general-information');
-  }
-
-  public function save(){
-    dd($this->numberContainers);
   }
 }
