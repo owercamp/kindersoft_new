53af4f990f97b1f72e557b2227970cd71e12d5cf
Ower A Campos <owerion22@gmail.com>
2024-01-05 10:23:54 -0500

    feat: se implementa el modelo states con migración, seeder y controller

---
+++ app/Models/States.php
@@ -0,0 +1,49 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\HasOne;
+
+class States extends Model
+{
+  use HasFactory;
+
+  /**
+   * The primary key associated with the table.
+   *
+   * @var string
+   */
+  protected $primaryKey = 'id';
+
+  /**
+   * The attributes that are mass assignable.
+   *
+   * @var array
+   */
+  protected $fillable = ['name', 'country_id'];
+
+  /**
+   * The attributes that should be cast to native types.
+   *
+   * @var array
+   */
+  protected $casts = [
+    'id' => 'integer',
+    'country_id' => 'integer',
+    'name' => 'string'
+  ];
+
+  /**
+   * Indicates if the model should be timestamped.
+   *
+   * @var bool
+   */
+  public $timestamps = true;
+
+  public function country() : HasOne
+  {
+    return $this->hasOne(Countries::class, 'id', 'country_id');
+  }
+}
