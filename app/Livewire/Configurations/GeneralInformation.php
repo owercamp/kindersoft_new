fb30b8d570c80b62287c9e8e95e6768383c24030
Ower A Campos <owerion22@gmail.com>
2024-01-20 15:39:16 -0500

    refactor: se agregan en metodo mount y se asignan valores a las propiedades del componente

--- app/Livewire/Configurations/GeneralInformation.php
+++ app/Livewire/Configurations/GeneralInformation.php
@@ -2,10 +2,32 @@
 
 namespace App\Livewire\Configurations;
 
+use App\Models\Country;
 use Livewire\Component;
 
 class GeneralInformation extends Component
 {
+
+  public $company;
+  public $commercial;
+  public $nit;
+  public $dv;
+  public $email;
+  public $website;
+  public $count;
+  public $countrys = [];
+
+  public function mount()
+  {
+    $this->company = "";
+    $this->commercial = "";
+    $this->nit = "";
+    $this->dv = "";
+    $this->email = "";
+    $this->website = "";
+    $this->count = 0;
+    $this->countrys = Country::pluck('name', 'id')->sort()->toArray();
+  }
 
   public function render()
   {
