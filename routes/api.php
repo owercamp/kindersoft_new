b3d3bf60cd5ee95eef052315b82d45f02c2a82bf
Ower A Campos <owerion22@gmail.com>
2024-01-20 15:33:09 -0500

    feat:se agrega la ruta de la api para los departamentos

--- routes/api.php
+++ routes/api.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Models\Departament;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
@@ -17,3 +18,8 @@ use Illuminate\Support\Facades\Route;
 Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
 });
+
+Route::post('/departaments',function(Request $request){
+    $departaments = Departament::where('country_id',$request->id_country)->pluck('name','id');
+    return $departaments;
+})->name('departaments');
