feda70a885adc1cc6c3ea0511c906f3e020fc0a3
Ower A Campos <owerion22@gmail.com>
2024-01-21 15:58:07 -0500

    feat: se crea la migracion y el modelo de regime

---
+++ app/Models/Regime.php
@@ -0,0 +1,42 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Regime extends Model
+{
+    use HasFactory;
+
+    /**
+     * The primary key associated with the table.
+     *
+     * @var string
+     */
+    protected $primaryKey = 'id';
+
+    /**
+     * The attributes that are mass assignable.
+     *
+     * @var array
+     */
+    protected $fillable = ['name'];
+
+    /**
+     * Indicates if the model should be timestamped.
+     *
+     * @var bool
+     */
+    public $timestamps = true;
+
+    /**
+     * The attributes that should be cast to native types.
+     *
+     * @var array
+     */
+    protected $casts = [
+      'id' => 'integer',
+      'name' => 'string',
+    ];
+}
