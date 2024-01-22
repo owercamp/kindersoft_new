81a81733cdc3201006fc696b2fb6d923876f0ede
Ower A Campos <owerion22@gmail.com>
2024-01-22 11:18:13 -0500

    feat: se implementa sweetalert2

--- resources/js/app.js
+++ resources/js/app.js
@@ -2,8 +2,10 @@ import './bootstrap';
 
 import Alpine from 'alpinejs';
 import focus from '@alpinejs/focus';
+import Swal from 'sweetalert2';
 import './main';
 window.Alpine = Alpine;
+window.Swal = Swal;
 
 Alpine.plugin(focus);
 Alpine.start();
