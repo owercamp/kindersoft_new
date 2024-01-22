ae36392ccf1352207cd10c65f256c276b551dcd7
Ower A Campos <owerion22@gmail.com>
2024-01-22 13:00:50 -0500

    feat: se crea el formulario por componente de las imagenes corporativas

---
+++ resources/views/livewire/informations/corporate-images.blade.php
@@ -0,0 +1,56 @@
+<div>
+  <x-slot name="header">
+    <h2 class="font-semibold text-xl text-gray-800 dark:text-gray-200 leading-tight text-end px-4">
+      {{ __('Configuration') }} > {{ __('Kindergarten Information') }} > {{ __('Corporate Images') }}
+    </h2>
+  </x-slot>
+  <div class="py-8">
+    <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
+      <div class="bg-white dark:bg-gray-800 overflow-hidden shadow-xl sm:rounded-lg py-8 px-5">
+        <div class="flex flex-row gap-2 justify-center py-2">
+          <div class="flex flex-col items-center bg-white border border-gray-200 rounded-lg shadow md:flex-row md:max-w-xl hover:bg-gray-100 dark:border-gray-700 dark:bg-gray-800 dark:hover:bg-gray-700">
+            <img class="object-cover w-full rounded-tl-lg rounded-bl-lg" style="height: 250px!important;" src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQmUmWHXPIc0Z3x1m0EF13NQmf_Tmor8xp9az21M0PoxA&s" alt="">
+            <div class="flex flex-col justify-between p-4 leading-normal">
+              <h5 class="mb-2 text-2xl font-bold tracking-tight text-gray-900 dark:text-white">{{ __('Corporate Logo') }}</h5>
+              <input type="file" class="mb-3 font-normal text-gray-700 dark:text-gray-400">
+            </div>
+          </div>
+          <div class="flex flex-col items-center bg-white border border-gray-200 rounded-lg shadow md:flex-row md:max-w-xl hover:bg-gray-100 dark:border-gray-700 dark:bg-gray-800 dark:hover:bg-gray-700">
+            <img class="object-cover w-full rounded-tl-lg rounded-bl-lg" style="height: 250px!important;" src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQmUmWHXPIc0Z3x1m0EF13NQmf_Tmor8xp9az21M0PoxA&s" alt="">
+            <div class="flex flex-col justify-between p-4 leading-normal">
+              <h5 class="mb-2 text-2xl font-bold tracking-tight text-gray-900 dark:text-white">{{ __('QR Code Web Page') }}</h5>
+              <input type="file" class="mb-3 font-normal text-gray-700 dark:text-gray-400">
+            </div>
+          </div>
+        </div>
+        <div class="flex flex-row gap-2 justify-center py-2">
+          <div class="flex flex-col items-center bg-white border border-gray-200 rounded-lg shadow md:flex-row md:max-w-xl hover:bg-gray-100 dark:border-gray-700 dark:bg-gray-800 dark:hover:bg-gray-700">
+            <img class="object-cover w-full rounded-tl-lg rounded-bl-lg" style="height: 250px!important;" src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQmUmWHXPIc0Z3x1m0EF13NQmf_Tmor8xp9az21M0PoxA&s" alt="">
+            <div class="flex flex-col justify-between p-4 leading-normal">
+              <h5 class="mb-2 text-2xl font-bold tracking-tight text-gray-900 dark:text-white">{{ __('QR Code Admission Form') }}</h5>
+              <input type="file" class="mb-3 font-normal text-gray-700 dark:text-gray-400">
+            </div>
+          </div>
+          <div class="flex flex-col items-center bg-white border border-gray-200 rounded-lg shadow md:flex-row md:max-w-xl hover:bg-gray-100 dark:border-gray-700 dark:bg-gray-800 dark:hover:bg-gray-700">
+            <img class="object-cover w-full rounded-tl-lg rounded-bl-lg" style="height: 250px!important;" src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQmUmWHXPIc0Z3x1m0EF13NQmf_Tmor8xp9az21M0PoxA&s" alt="">
+            <div class="flex flex-col justify-between p-4 leading-normal">
+              <h5 class="mb-2 text-2xl font-bold tracking-tight text-gray-900 dark:text-white">{{ __('QR Code School Agenda') }}</h5>
+              <input type="file" class="mb-3 font-normal text-gray-700 dark:text-gray-400">
+            </div>
+          </div>
+        </div>
+        <div class="flex flex-row gap-2 justify-center py-2">
+          <div class="flex flex-col items-center bg-white border border-gray-200 rounded-lg shadow md:flex-row md:max-w-xl hover:bg-gray-100 dark:border-gray-700 dark:bg-gray-800 dark:hover:bg-gray-700">
+            <img class="object-cover w-full rounded-tl-lg rounded-bl-lg" style="height: 250px!important;" src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQmUmWHXPIc0Z3x1m0EF13NQmf_Tmor8xp9az21M0PoxA&s" alt="">
+            <div class="flex flex-col justify-between p-4 leading-normal">
+              <h5 class="mb-2 text-2xl font-bold tracking-tight text-gray-900 dark:text-white">{{ __('QR Code Virtual Platform') }}</h5>
+              <input type="file"class="mb-3 font-normal text-gray-700 dark:text-gray-400">
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
+</div>
+
+</div>
