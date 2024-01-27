2886cc751e1fbb229d30971df5249067ce606b8c
Ower A Campos <owerion22@gmail.com>
2024-01-27 08:39:23 -0500

    refactor: se agrega configuración de directorio para archivos en servidor

--- config/filesystems.php
+++ config/filesystems.php
@@ -44,6 +44,15 @@ return [
             'throw' => false,
         ],
 
+        // configuracion disk para el servidor
+        // 'public' => [
+        //     'driver' => 'local',
+        //     'root' => dirname('./public/storage'),
+        //     'url' => env('APP_URL').'/storage',
+        //     'visibility' => 'public',
+        //     'throw' => false,
+        // ],
+
         's3' => [
             'driver' => 's3',
             'key' => env('AWS_ACCESS_KEY_ID'),
