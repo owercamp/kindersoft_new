bddb0d6722efb3b8d358c9222f082c84d0e5a800
Ower A Campos <owerion22@gmail.com>
2024-01-27 08:32:06 -0500

    refactor: se corrigue suplicidad de enunciado

--- resources/views/auth/forgot-password.blade.php
+++ resources/views/auth/forgot-password.blade.php
@@ -10,9 +10,6 @@
             <div class="mb-4 text-sm text-gray-600 dark:text-gray-400">
               {{ __('Forgot your password? No problem. Just let us know your email address and we will email you a password reset link that will allow you to choose a new one.') }}
             </div>
-            <div class="mb-4 text-sm text-gray-600 dark:text-gray-400">
-              {{ __('Forgot your password? No problem. Just let us know your email address and we will email you a password reset link that will allow you to choose a new one.') }}
-            </div>
 
             @if (session('status'))
             <div class="mb-4 font-medium text-sm text-green-600 dark:text-green-400">
