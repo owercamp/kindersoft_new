074cb443cfc6f03d1642e2a81e3859d5c38b7673
Ower A Campos <owerion22@gmail.com>
2024-02-13 10:33:45 -0500

    refactor: implementa tema claro en footer

--- resources/views/components/footer.blade.php
+++ resources/views/components/footer.blade.php
@@ -1,4 +1,4 @@
-<footer class="fixed w-full bottom-0 left-0 z-30 p-4 bg-gray-900 text-orange-600">
+<footer class="fixed w-full bottom-0 left-0 z-30 p-4 bg-blue-900 text-slate-900 dark:bg-gray-900 dark:text-orange-600">
   <div class="text-center">
     <p>Copyright <span class="text-white">©</span> {{now()->year}} - Todos los Derechos Reservados - <a class="font-bold" href="mailto:gerencia@javapri.com.co"><span class="text-white">JAVA</span>PRI</a> - <a href="https://javapri.co/" target="_blank">www.javapri.co</a></p>
   </div>
