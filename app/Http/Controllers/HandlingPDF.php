1a514fc81a3cb23b6f412fb6b0aad2f4b3ea224f
Ower A Campos <owerion22@gmail.com>
2024-02-26 07:30:23 -0500

    feat: se agrega function pdf_cooperative

--- app/Http/Controllers/HandlingPDF.php
+++ app/Http/Controllers/HandlingPDF.php
@@ -14,7 +14,7 @@ use Illuminate\Support\Facades\Storage;
 
 class HandlingPDF extends Controller
 {
-  public function pdf_admissions(Request $request)
+  public function pdf_commercial(Request $request)
   {
     $logo = filter_var(CorporateImages::where('name', 'Logo Corporativo')->value('url_image'), FILTER_SANITIZE_URL);
     if(config('app.env') == 'prod'){
@@ -32,6 +32,25 @@ class HandlingPDF extends Controller
     $pdf = App::make('dompdf.wrapper');
     $pdf->loadView('templatePDF.admissionsPDF', compact('image', 'reason', 'proposal', 'date','consecutive','headquarters'))->setPaper('a4', 'landscape');
     return $pdf->stream();
-    dd('hi');
+  }
+
+  public function pdf_cooperative(Request $request)
+  {
+    $logo = filter_var(CorporateImages::where('name', 'Logo Corporativo')->value('url_image'), FILTER_SANITIZE_URL);
+    if(config('app.env') == 'prod'){
+      $image = str_replace('\\', '/', str_replace('/storage/', '', Storage::disk('public')->url($logo)));
+    }else{
+      $image = str_replace('\\', '/', str_replace('/storage/', '/', Storage::disk('public')->url($logo)));
+    }
+    $reason = GeneralInformation::where('id', 1)->select('company', 'commercial')->get();
+    $headquarters = Headquarter::where('company_id', 1)->select('headquarters')->get();
+    $proposal = __('Educational Cooperation Contract');
+    $locale = config('app.locale', 'en');
+    $now = Carbon::now();
+    $date = ucfirst(Carbon::parse($now)->locale($locale)->isoFormat('dddd')) . ', ' . Carbon::parse($now)->locale($locale)->isoFormat('DD') . ' ' . __('of') . ' ' . Carbon::parse($now)->locale($locale)->isoFormat('MMMM') . ' ' . __('of') . ' ' . Carbon::parse($now)->locale($locale)->isoFormat('YYYY');
+    $consecutive = TaxInformation::where('id', 1)->value('next_invoice');
+    $pdf = App::make('dompdf.wrapper');
+    $pdf->loadView('templatePDF.admissionsPDF', compact('image', 'reason', 'proposal', 'date','consecutive','headquarters'))->setPaper('a4', 'landscape');
+    return $pdf->stream();
   }
 }
