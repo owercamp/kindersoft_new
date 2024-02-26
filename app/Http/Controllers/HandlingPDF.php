205b632c01d14ce021d02956993a267aa541a079
Ower A Campos <owerion22@gmail.com>
2024-02-25 21:34:19 -0500

    feat: se agrega controlador para los pdfs

---
+++ app/Http/Controllers/HandlingPDF.php
@@ -0,0 +1,37 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Carbon\Carbon;
+use App\Models\Headquarter;
+use Illuminate\Http\Request;
+use App\Models\TaxInformation;
+use App\Models\CorporateImages;
+use App\Models\GeneralInformation;
+use Illuminate\Support\Facades\App;
+use Barryvdh\DomPDF\Facade\Pdf as PDF;
+use Illuminate\Support\Facades\Storage;
+
+class HandlingPDF extends Controller
+{
+  public function pdf_admissions(Request $request)
+  {
+    $logo = filter_var(CorporateImages::where('name', 'Logo Corporativo')->value('url_image'), FILTER_SANITIZE_URL);
+    if(config('app.env') == 'prod'){
+      $image = str_replace('\\', '/', str_replace('/storage/', '', Storage::disk('public')->url($logo)));
+    }else{
+      $image = str_replace('\\', '/', str_replace('/storage/', '/', Storage::disk('public')->url($logo)));
+    }
+    $reason = GeneralInformation::where('id', 1)->select('company', 'commercial')->get();
+    $headquarters = Headquarter::where('company_id', 1)->select('headquarters')->get();
+    $proposal = __('Commercial Proposal');
+    $locale = config('app.locale', 'en');
+    $now = Carbon::now();
+    $date = ucfirst(Carbon::parse($now)->locale($locale)->isoFormat('dddd')) . ', ' . Carbon::parse($now)->locale($locale)->isoFormat('DD') . ' ' . __('of') . ' ' . Carbon::parse($now)->locale($locale)->isoFormat('MMMM') . ' ' . __('of') . ' ' . Carbon::parse($now)->locale($locale)->isoFormat('YYYY');
+    $consecutive = TaxInformation::where('id', 1)->value('next_invoice');
+    $pdf = App::make('dompdf.wrapper');
+    $pdf->loadView('templatePDF.admissionsPDF', compact('image', 'reason', 'proposal', 'date','consecutive','headquarters'))->setPaper('a4', 'landscape');
+    return $pdf->stream();
+    dd('hi');
+  }
+}
