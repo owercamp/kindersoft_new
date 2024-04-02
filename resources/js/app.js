50245f28fc674fd1aced66c13e10cdf382ada386
Ower A Campos <owerion22@gmail.com>
2024-04-02 15:25:53 -0500

    refactor: se comentan las lineas de importe, windows y inicio de alpinejs ya que no es requerido

--- resources/js/app.js
+++ resources/js/app.js
@@ -1,12 +1,12 @@
 import './bootstrap';
 
-import Alpine from 'alpinejs';
+// import Alpine from 'alpinejs';
 import focus from '@alpinejs/focus';
 import Swal from 'sweetalert2';
 import './main';
-window.Alpine = Alpine;
+// window.Alpine = Alpine;
 window.Swal = Swal;
 
 Alpine.plugin(focus);
-Alpine.start();
+// Alpine.start();
 
