420ed065aabfe18a1a20087d32fde98adcb29c87
Ower A Campos <owerion22@gmail.com>
2024-04-03 14:29:58 -0500

    feat: se crean el componente y la vista para provvedores de salud

---
+++ app/Livewire/Configurations/HealthCareProvider.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace App\Livewire\Configurations;
+
+use Livewire\Component;
+
+class HealthCareProvider extends Component
+{
+    public function render()
+    {
+        return view('livewire.configurations.health-care-provider');
+    }
+}
