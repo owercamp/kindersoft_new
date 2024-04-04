07aa429e62e33e8e3d05c9d04814bc52e1eaeb9a
Ower A Campos <owerion22@gmail.com>
2024-04-03 20:05:27 -0500

    feat: se crean la vista y el componente cargos laborales

---
+++ app/Livewire/Configurations/EmploymentPositions.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace App\Livewire\Configurations;
+
+use Livewire\Component;
+
+class EmploymentPositions extends Component
+{
+    public function render()
+    {
+        return view('livewire.configurations.employment-positions');
+    }
+}
