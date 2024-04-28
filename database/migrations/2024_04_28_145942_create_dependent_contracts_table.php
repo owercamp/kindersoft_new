de094d53f0631740804725087fbbf578592f2625
Ower A. Campos <owerion22@gmail.com>
2024-04-28 11:00:26 -0500

    feat: se agregan migracion y modelo para contrato dependiente

---
+++ database/migrations/2024_04_28_145942_create_dependent_contracts_table.php
@@ -0,0 +1,32 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+  /**
+   * Run the migrations.
+   */
+  public function up(): void
+  {
+    Schema::create('dependent_contracts', function (Blueprint $table) {
+      $table->id();
+      $table->foreignId('attendant_id')->nullable()->constrained('attendants', 'id')->onUpdate('cascade')->onDelete('set null');
+      $table->string('company');
+      $table->string('nit');
+      $table->foreignId('position_id')->nullable()->constrained('employment_positions', 'id')->onUpdate('cascade')->onDelete('set null');
+      $table->date('date_entry');
+      $table->timestamps();
+    });
+  }
+
+  /**
+   * Reverse the migrations.
+   */
+  public function down(): void
+  {
+    Schema::dropIfExists('dependent_contracts');
+  }
+};
