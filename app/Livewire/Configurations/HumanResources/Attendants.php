ffa500307e7f31f8a4f6a66d583e5e761c6be088
Ower A. Campos <owerion22@gmail.com>
2024-04-29 18:44:54 -0500

    refactor: configuraciones iniciales del componente acudiente

--- app/Livewire/Configurations/HumanResources/Attendants.php
+++ app/Livewire/Configurations/HumanResources/Attendants.php
@@ -7,14 +7,17 @@ use App\Models\Genre;
 use App\Models\Postal;
 use App\Models\Country;
 use Livewire\Component;
+use App\Models\Attendant;
 use App\Models\Bloodtype;
 use App\Models\Departament;
 use App\Models\Municipality;
 use App\Models\Neighborhood;
 use App\Models\AcademicLevel;
+use App\Models\DependentContract;
 use App\Models\EmploymentPosition;
 use App\Models\TypeIdentification;
 use Illuminate\Support\Facades\DB;
+use App\Models\IndependentContract;
 
 class Attendants extends Component
 {
@@ -30,6 +33,46 @@ class Attendants extends Component
   public $genres, $genre, $type_bloods, $other_genre, $other, $blood;
   public $contract, $dependent, $independent, $address, $phone, $nationality, $indep_text;
   public $positions, $dep_company, $dep_nit, $dep_position, $dep_date_entry;
+  public $registerPosition, $document;
+  public $modal = false;
+  public $arrayEdit = [
+    'register' => '',
+    'identification' => '',
+    'number_document' => '',
+    'firstname' => '',
+    'middlename' => '',
+    'lastname' => '',
+    'middlelastname' => '',
+    'email' => '',
+    'academic' => '',
+    'other_title' => false,
+    'title_academic' => '',
+    'country' => '',
+    'department' => '',
+    'municipality' => '',
+    'city' => '',
+    'location' => '',
+    'postal' => '',
+    'address' => '',
+    'phone' => '',
+    'nationality' => '',
+    'genre' => '',
+    'other' => false,
+    'other_genre' => '',
+    'blood' => '',
+    'contract' => '',
+    'dependent' => false,
+    'independent' => false,
+    'indep_text' => '',
+    'dep_company' => '',
+    'dep_nit' => '',
+    'dep_position' => '',
+    'dep_date_entry' => '',
+    'position' => '',
+    'id' => ''
+  ];
+
+  protected $listeners = ['successPosition' => 'incrementPosition', 'success' => 'increment', 'changes_data' => 'render'];
 
   public function mount()
   {
@@ -52,8 +95,41 @@ class Attendants extends Component
 
   public function increment()
   {
-    $registers = DB::table('collaborators')->latest('register')->value('register') + 1;
+    $registers = DB::table('attendants')->latest('register')->value('register') + 1;
     $this->register = str_pad($registers, 4, '0', STR_PAD_LEFT);
+  }
+
+  public function incrementPosition()
+  {
+    $register = DB::table('employment_positions')->latest('register')->value('register') + 1;
+    $this->registerPosition = str_pad($register, 4, '0', STR_PAD_LEFT);
+  }
+
+  public function savePosition()
+  {
+    $this->validate([
+      'register' => 'required|numeric',
+      'document' => 'required|string|max:30'
+    ], [], [
+      'register' => __('Registration'),
+      'document' => __('Document Name')
+    ]);
+
+    $register = new EmploymentPosition();
+    $register->register = $this->register;
+    $register->name = $this->document;
+    if ($register->save()) {
+      $this->dispatch('swal:modal', [
+        'type' => 'success',
+        'message' => __('Successfully Created Record'),
+        'timer' => 1500,
+        'showConfirmButton' => false,
+        'success' => true
+      ]);
+
+      $this->dispatch('successPosition');
+      $this->positions = EmploymentPosition::pluck('name', 'id');
+    }
   }
 
   public function contract_change()
@@ -199,7 +275,7 @@ class Attendants extends Component
         'dep_nit' => 'required|string',
         'dep_position' => 'required|numeric|exists:employment_positions,id',
         'dep_date_entry' => 'required|date'
-      ],[],[
+      ], [], [
         'dep_company' => __('Company'),
         'dep_nit' => __('NIT'),
         'dep_position' => __('Position'),
@@ -208,16 +284,122 @@ class Attendants extends Component
     } elseif ($this->contract == "INDEPENDIENTE") {
       $this->validate([
         'indep_text' => 'required|max:500'
-      ],[],[
+      ], [], [
         'indep_text' => __('Date of Entry')
       ]);
     }
 
-    dd('success');
+    DB::beginTransaction();
+    try {
+      $register = new Attendant();
+      $register->register = trim($this->register);
+      $register->type_identification_id = $this->identification;
+      $register->number_document = trim($this->number_document);
+      $register->firstname = trim($this->firstname);
+      $register->middlename = trim($this->middlename);
+      $register->lastname = trim($this->lastname);
+      $register->middlelastname = trim($this->middlelastname);
+      $register->country_id = $this->country;
+      $register->department_id = $this->department;
+      $register->municipality_id = $this->municipality;
+      $register->city_id = $this->city;
+      $register->location_id = $this->location;
+      $register->postal_id = $this->postal;
+      $register->address = trim($this->address);
+      $register->phone = trim($this->phone);
+      $register->email = trim($this->email);
+      $register->nationality_id = $this->nationality;
+      $register->genre_id = $this->genre;
+
+      if ($this->genre == 3) {
+        $register->genre_text = $this->other_genre;
+      }
+
+      $register->academic_id = $this->academic;
+
+      if ($this->academic == 4) {
+        $register->academic_text = $this->title_academic;
+      }
+
+      $register->bloodtype_id = $this->blood;
+      $register->contract = $this->contract;
+      if ($register->save()) {
+        if ($this->contract == "DEPENDIENTE") {
+          $contracts = new DependentContract();
+          $contracts->attendant_id = $register->id;
+          $contracts->company = trim($this->dep_company);
+          $contracts->nit = trim($this->dep_nit);
+          $contracts->position_id = $this->dep_position;
+          $contracts->date_entry = $this->dep_date_entry;
+          $contracts->save();
+        } elseif ($this->contract == "INDEPENDIENTE") {
+          $contracts_independent = new IndependentContract();
+          $contracts_independent->attendant_id = $register->id;
+          $contracts_independent->description = trim($this->indep_text);
+          $contracts_independent->save();
+        }
+
+        $this->dispatch('swal:modal', [
+          'type' => 'success',
+          'message' => __('Successfully Created Record'),
+          'timer' => 1500,
+          'showConfirmButton' => false,
+          'success' => 'completed'
+        ]);
+
+        $this->reset([
+          'identification',
+          'number_document',
+          'firstname',
+          'middlename',
+          'lastname',
+          'middlelastname',
+          'country',
+          'department',
+          'municipality',
+          'city',
+          'location',
+          'postal',
+          'address',
+          'phone',
+          'email',
+          'nationality',
+          'genre',
+          'other_genre',
+          'academic',
+          'title_academic',
+          'blood',
+          'contract',
+          'dep_company',
+          'dep_nit',
+          'dep_position',
+          'dep_date_entry',
+          'indep_text'
+        ]);
+        $this->dispatch('success');
+        $this->dispatch('changes_data');
+      }
+      DB::commit();
+    } catch (\Exception $e) {
+      DB::rollBack();
+      $this->dispatch('swal:modal', [
+        'type' => 'error',
+        'message' => $e->getMessage(),
+        'timer' => 4000,
+        'showConfirmButton' => true
+      ]);
+    }
+  }
+
+  public function openModal($id)
+  {
+    $register = Attendant::with('academic', 'neighborhood','country','department','municipality','city','postal','contract_dependent','contract_independent','bloodtype','genre','nationality','identification')->find($id);
+    $this->modal = true;
   }
 
   public function render()
   {
-    return view('livewire.configurations.human-resources.attendants');
+    $attendants = Attendant::with('academic', 'neighborhood')->paginate(15);
+    return view('livewire.configurations.human-resources.attendants', compact('attendants'));
   }
 }
