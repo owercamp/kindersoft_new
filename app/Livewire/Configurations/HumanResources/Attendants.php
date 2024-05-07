f49baf51702ce279e2c0f24de162dcf3201f1a72
Ower A Campos <owerion22@gmail.com>
2024-05-06 21:12:53 -0500

    feat: se agrega componente attendant

--- app/Livewire/Configurations/HumanResources/Attendants.php
+++ app/Livewire/Configurations/HumanResources/Attendants.php
@@ -24,7 +24,7 @@ class Attendants extends Component
 
   public $register, $type_ids, $identification, $number_document, $firstname, $middlename, $lastname, $middlelastname, $email;
   public $academic, $academics, $other_title, $title_academic;
-  public $country, $countrys, $current_country;
+  public $country, $countrys, $current_country, $current_country_id;
   public $department, $departments, $current_department;
   public $municipality, $municipalities, $current_municipality;
   public $city, $cities, $current_city;
@@ -135,13 +135,19 @@ class Attendants extends Component
   public function contract_change()
   {
     $this->dependent = false;
+    $this->arrayEdit['dependent'] = false;
     $this->independent = false;
-    switch ($this->contract) {
+    $this->arrayEdit['independent'] = false;
+
+    $expr = ($this->contract) ? $this->contract : $this->arrayEdit['contract'];
+    switch ($expr) {
       case "DEPENDIENTE":
         $this->dependent = true;
+        $this->arrayEdit['dependent'] = true;
         break;
       case "INDEPENDIENTE":
         $this->independent = true;
+        $this->arrayEdit['independent'] = true;
         break;
     }
   }
@@ -149,7 +155,7 @@ class Attendants extends Component
   public function change_country($from = "")
   {
     if ($from) {
-      $this->departments = Departament::where('country_id', $this->editArray['country'])->orderby('name')->pluck('name', 'id');
+      $this->departments = Departament::where('country_id', $this->arrayEdit['country'])->orderby('name')->pluck('name', 'id');
     } else {
       $this->departments = Departament::where('country_id', $this->country)->orderby('name')->pluck('name', 'id');
     }
@@ -158,7 +164,7 @@ class Attendants extends Component
   public function change_department($from = "")
   {
     if ($from) {
-      $this->municipalities = Municipality::where('departament_id', $this->editArray['department'])->orderby('name')->pluck('name', 'id');
+      $this->municipalities = Municipality::where('departament_id', $this->arrayEdit['department'])->orderby('name')->pluck('name', 'id');
     } else {
       $this->municipalities = Municipality::where('departament_id', $this->department)->orderby('name')->pluck('name', 'id');
     }
@@ -167,7 +173,7 @@ class Attendants extends Component
   public function change_municipality($from = "")
   {
     if ($from) {
-      $this->cities = City::where('municipality_id', $this->editArray['municipality'])->orderby('name')->pluck('name', 'id');
+      $this->cities = City::where('municipality_id', $this->arrayEdit['municipality'])->orderby('name')->pluck('name', 'id');
     } else {
       $this->cities = City::where('municipality_id', $this->municipality)->orderby('name')->pluck('name', 'id');
     }
@@ -176,7 +182,7 @@ class Attendants extends Component
   public function change_city($from = "")
   {
     if ($from) {
-      $this->locations = Neighborhood::where('city_id', $this->editArray['city'])->orderby('name')->pluck('name', 'id');
+      $this->locations = Neighborhood::where('city_id', $this->arrayEdit['city'])->orderby('name')->pluck('name', 'id');
     } else {
       $this->locations = Neighborhood::where('city_id', $this->city)->orderby('name')->pluck('name', 'id');
     }
@@ -185,7 +191,7 @@ class Attendants extends Component
   public function change_location($from = "")
   {
     if ($from) {
-      $this->postals = Postal::where('neighborhood_id', $this->editArray['location'])->orderby('name')->pluck('name', 'id');
+      $this->postals = Postal::where('neighborhood_id', $this->arrayEdit['location'])->orderby('name')->pluck('name', 'id');
     } else {
       $this->postals = Postal::where('neighborhood_id', $this->location)->orderby('name')->pluck('name', 'id');
     }
@@ -194,15 +200,13 @@ class Attendants extends Component
   public function change_academic()
   {
     $this->other_title = ($this->academic == 4) ? true : false;
+    $this->arrayEdit['other_title'] = ($this->arrayEdit['academic'] == 4) ? true : false;
   }
 
   function change_genre()
   {
-    if ($this->genre == 3) {
-      $this->other = true;
-    } else {
-      $this->other = false;
-    }
+    $this->other = ($this->genre == 3) ? true : false;
+    $this->arrayEdit['other'] = ($this->arrayEdit['genre'] == 3) ? true : false;
   }
 
   public function save()
@@ -393,8 +397,182 @@ class Attendants extends Component
 
   public function openModal($id)
   {
-    $register = Attendant::with('academic', 'neighborhood','country','department','municipality','city','postal','contract_dependent','contract_independent','bloodtype','genre','nationality','identification')->find($id);
+    $register = Attendant::with('academic', 'neighborhood', 'country', 'department', 'municipality', 'city', 'postal', 'contract_dependent', 'contract_independent', 'bloodtype', 'genre', 'nationality', 'identification')->find($id);
+
+    $this->current_country_id = $register->country_id;
+    $this->current_country = Country::where('id', $register->country_id)->value('name');
+    $this->current_department = Departament::where('id', $register->department_id)->value('name');
+    $this->current_municipality = Municipality::where('id', $register->municipality_id)->value('name');
+    $this->current_city = City::where('id', $register->city_id)->value('name');
+    $this->current_location = Neighborhood::where('id', $register->location_id)->value('name');
+    $this->current_postal = Postal::where('id', $register->postal_id)->value('name');
+
+    $this->arrayEdit['register'] = str_pad($register->register, 4, "0", STR_PAD_LEFT);
+    $this->arrayEdit['identification'] = $register->type_identification_id;
+    $this->arrayEdit['number_document'] = $register->number_document;
+    $this->arrayEdit['firstname'] = $register->firstname;
+    $this->arrayEdit['middlename'] = $register->middlename;
+    $this->arrayEdit['lastname'] = $register->lastname;
+    $this->arrayEdit['middlelastname'] = $register->middlelastname;
+    $this->arrayEdit['country'] = $register->country_id;
+    $this->arrayEdit['department'] = $register->department_id;
+    $this->arrayEdit['municipality'] = $register->municipality_id;
+    $this->arrayEdit['city'] = $register->city_id;
+    $this->arrayEdit['address'] = $register->address;
+    $this->arrayEdit['location'] = $register->location_id;
+    $this->arrayEdit['postal'] = $register->postal_id;
+    $this->arrayEdit['phone'] = $register->phone;
+    $this->arrayEdit['position'] = $register->contract_dependent->position_id;
+    $this->arrayEdit['email'] = $register->email;
+    $this->arrayEdit['nationality'] = $register->nationality_id;
+    $this->arrayEdit['genre'] = $register->genre_id;
+    $this->arrayEdit['id'] = $register->id;
+
+    if ($register->genre_id == 3) {
+      $this->arrayEdit['other'] = true;
+      $this->arrayEdit['other_genre'] = $register->genre_text;
+    }
+
+    $this->arrayEdit['academic'] = $register->academic_id;
+    if ($register->academic_id == 4) {
+      $this->arrayEdit['other_title'] = true;
+      $this->arrayEdit['title_academic'] = $register->academic_text;
+    }
+
+    $this->arrayEdit['blood'] = $register->bloodtype_id;
+
+    $this->arrayEdit['contract'] = $register->contract;
+
+    switch ($register->contract) {
+      case 'DEPENDIENTE':
+        $this->arrayEdit['dependent'] = true;
+        $this->arrayEdit['dep_company'] = $register->contract_dependent->company;
+        $this->arrayEdit['dep_nit'] = $register->contract_dependent->nit;
+        $this->arrayEdit['dep_position'] = $register->contract_dependent->position_id;
+        $this->arrayEdit['dep_date_entry'] = $register->contract_dependent->date_entry;
+        break;
+
+      case 'INDEPENDIENTE':
+        $this->arrayEdit['independent'] = true;
+        $this->arrayEdit['indep_text'] = $register->contract_independent->description;
+        break;
+    }
     $this->modal = true;
+  }
+
+  public function edit()
+  {
+    $this->validate([
+      'arrayEdit.register' => 'required|numeric',
+      'arrayEdit.identification' => 'required|numeric|exists:type_identifications,id',
+      'arrayEdit.number_document' => 'required|numeric|max_digits:16',
+      'arrayEdit.firstname' => 'required|string',
+      'arrayEdit.middlename' => 'nullable|string',
+      'arrayEdit.lastname' => 'required|string',
+      'arrayEdit.middlelastname' => 'nullable|string',
+      'arrayEdit.country' => 'required|numeric|exists:countries,id',
+      'arrayEdit.department' => 'required|numeric|exists:departaments,id',
+      'arrayEdit.municipality' => 'required|numeric|exists:municipalities,id',
+      'arrayEdit.city' => 'required|numeric|exists:cities,id',
+      'arrayEdit.location' => 'required|numeric|exists:neighborhoods,id',
+      'arrayEdit.postal' => 'required|numeric|exists:postals,id',
+      'arrayEdit.address' => 'required|string',
+      'arrayEdit.phone' => 'required|numeric|min:10',
+      'arrayEdit.email' => 'required|email',
+      'arrayEdit.nationality' => 'required|numeric|exists:countries,id',
+      'arrayEdit.genre' => 'required|numeric|exists:genres,id',
+      'arrayEdit.academic' => 'required|numeric|exists:academic_levels,id',
+      'arrayEdit.blood' => 'required|numeric|exists:bloodtypes,id',
+      'arrayEdit.contract' => 'required|string'
+    ], [], [
+      'arrayEdit.register' => __('Registration'),
+      'arrayEdit.identification' => __('Document Type'),
+      'arrayEdit.number_document' => __('Number') . " " . __('of') . " " . __('Document'),
+      'arrayEdit.firstname' => __('First Name'),
+      'arrayEdit.middlename' => __('Middle Name'),
+      'arrayEdit.lastname' => __('Last Name'),
+      'arrayEdit.middlelastname' => __('Middle Last Name'),
+      'arrayEdit.country' => __('Country'),
+      'arrayEdit.department' => __('Department'),
+      'arrayEdit.municipality' => __('Municipality'),
+      'arrayEdit.city' => __('City'),
+      'arrayEdit.location' => __('Location / Neighborhood'),
+      'arrayEdit.postal' => __('Zip Code'),
+      'arrayEdit.address' => __('Address'),
+      'arrayEdit.phone' => __('Phone'),
+      'arrayEdit.email' => __('Email address'),
+      'arrayEdit.nationality' => __('Nationality'),
+      'arrayEdit.genre' => __('Genre'),
+      'arrayEdit.academic' => __('Academic level'),
+      'arrayEdit.blood' => __('Bloodtype'),
+      'arrayEdit.contract' => __('Contract Work or Labor')
+    ]);
+
+    if ($this->arrayEdit['genre'] == 3) {
+      $this->validate([
+        'arrayEdit.other_genre' => 'required|string'
+      ], [
+        'arrayEdit.other_genre' => __('Please fill in the text box for gender')
+      ], []);
+    }
+
+    if ($this->arrayEdit['academic'] == 4) {
+      $this->validate([
+        'arrayEdit.title_academic' => 'required|string'
+      ], [
+        'arrayEdit.title_academic' => __('Please enter the title obtained')
+      ], []);
+    }
+
+    if ($this->arrayEdit['contract'] == "DEPENDIENTE") {
+      $this->validate([
+        'arrayEdit.dep_company' => 'required|string',
+        'arrayEdit.dep_nit' => 'required|string',
+        'arrayEdit.dep_position' => 'required|numeric|exists:employment_positions,id',
+        'arrayEdit.dep_date_entry' => 'required|date'
+      ], [], [
+        'arrayEdit.dep_company' => __('Company'),
+        'arrayEdit.dep_nit' => __('NIT'),
+        'arrayEdit.dep_position' => __('Position'),
+        'arrayEdit.dep_date_entry' => __('Date of Entry')
+      ]);
+    } elseif ($this->arrayEdit['contract'] == "INDEPENDIENTE") {
+      $this->validate([
+        'arrayEdit.indep_text' => 'required|max:500'
+      ], [], [
+        'arrayEdit.indep_text' => __('Date of Entry')
+      ]);
+    }
+    $register = Attendant::with('academic', 'neighborhood', 'country', 'department', 'municipality', 'city', 'postal', 'contract_dependent', 'contract_independent', 'bloodtype', 'genre', 'nationality', 'identification')->find($this->arrayEdit['id']);
+
+    dd($register);
+
+    $register->register = $this->arrayEdit['register'];
+    $register->type_identification_id = $this->arrayEdit['identification'];
+    $register->number_document = $this->arrayEdit['number_document'];
+    $register->firstname = $this->arrayEdit['firstname'];
+    $register->middlename = $this->arrayEdit['middlename'];
+    $register->lastname = $this->arrayEdit['lastname'];
+    $register->middlelastname = $this->arrayEdit['middlelastname'];
+    $register->country_id = ($this->arrayEdit['country']) ? $this->arrayEdit['country'] : $this->current_country_id;
+    $register->department_id = $this->arrayEdit['department'];
+    $register->municipality_id = $this->arrayEdit['municipality'];
+    $register->city_id = $this->arrayEdit['city'];
+    $register->location_id = $this->arrayEdit['location'];
+    $register->postal_id = $this->arrayEdit['postal'];
+    $register->address = $this->arrayEdit['address'];
+    $register->phone = $this->arrayEdit['phone'];
+    $register->email = $this->arrayEdit['email'];
+    $register->nationality_id = $this->arrayEdit['nationality'];
+    $register->genre = $this->arrayEdit['genre'];
+    if ($this->arrayEdit['genre'] != 3) {
+      $register->other_genre = '';
+    } elseif ($this->arrayEdit['genre'] == 3) {
+      $register->other_genre = $this->arrayEdit['other_genre'];
+    }
+
+
+    dd($register);
   }
 
   public function render()
