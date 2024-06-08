582d14aeb39836cb0726e1edaf90245725884186
Ower A. Campos <owerion22@gmail.com>
2024-06-08 08:56:02 -0500

    feat: se agrega vista para el componente proveedor

---
+++ resources/views/livewire/configurations/human-resources/providers.blade.php
@@ -0,0 +1,52 @@
+<div>
+  <x-slot name="header">
+    <h2 class="font-semibold text-xl text-gray-800 dark:text-gray-200 leading-tight text-end px-4">
+      {{ __('Configuration') }} > {{ __('Human Resources') }} > {{ __('Suppliers') }}
+    </h2>
+  </x-slot>
+  <div class="py-8">
+    <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
+      <div class="bg-white dark:bg-gray-800 overflow-hidden shadow-xl sm:rounded-lg py-6 px-5">
+
+      <div x-data="{ 'showModal': false }" class="flex justify-center">
+          <!-- Trigger for Modal -->
+          <x-button type="button" @click="showModal = true" wire:click="increment" class="bg-green-800 hover:bg-green-700 dark:bg-sky-800 dark:hover:bg-sky-600 text-white mx-auto flex flex-row">
+            <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="w-4 h-4">
+              <path stroke-linecap="round" stroke-linejoin="round" d="M12 4.5v15m7.5-7.5h-15" />
+            </svg>
+            {{ __('Create') }}</x-button>
+
+          <!-- Modal -->
+          <div class="fixed inset-0 z-50 flex items-center justify-center overflow-auto bg-black bg-opacity-50" x-show="showModal" id="modal">
+            <!-- Modal inner -->
+            <div class="max-w-3xl max-h-[42rem] px-6 py-4 mx-auto text-left text-gray-800 dark:text-gray-200 bg-white dark:bg-gray-800 rounded shadow-lg w-9/12 overflow-y-auto" x-transition:enter="motion-safe:ease-out duration-300" x-transition:enter-start="opacity-0 scale-90" x-transition:enter-end="opacity-100 scale-100">
+              <!-- Title / Close-->
+              <div class="flex items-center justify-between my-4">
+                <h5 class="mr-3 text-gray-800 dark:text-gray-200 max-w-none"> {{ __('Create') }} {{ __('Provider') }} </h5>
+
+                <button type="button" class="z-50 cursor-pointer" @click="showModal = false" id="closedModal">
+                  <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor">
+                    <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M6 18L18 6M6 6l12 12" />
+                  </svg>
+                </button>
+              </div>
+              <hr>
+
+              <!-- content -->
+              <div class="p-4">
+                <form wire:submit.prevent="save">
+                  @csrf
+                  @method('POST')
+                  @include('livewire.configurations.Partials.forms-provider')
+                  <div class="flex justify-end mt-1">
+                    <x-button class="bg-green-800 hover:bg-green-700 dark:bg-sky-800 dark:hover:bg-sky-600 w-full" type="submit">{{ __('Save') }}</x-button>
+                  </div>
+                </form>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
+</div>
