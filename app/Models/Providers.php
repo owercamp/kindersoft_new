135df7fd89960f004a1ae524f53cc4ac547f86ef
Ower A. Campos <owerion22@gmail.com>
2024-06-29 17:28:22 -0500

    refactor: se agrega relacion con el estado

--- app/Models/Providers.php
+++ app/Models/Providers.php
@@ -4,10 +4,10 @@ namespace App\Models;
 
 use App\Models\Legal;
 use App\Models\Person;
+use App\Models\StatesNames;
 use Illuminate\Database\Eloquent\Model;
-use Illuminate\Database\Eloquent\Relations\BelongsTo;
-use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Relations\HasOne;
+use Illuminate\Database\Eloquent\Factories\HasFactory;
 
 class Providers extends Model
 {
@@ -32,7 +32,7 @@ class Providers extends Model
    *
    * @var array<string>
    */
-  protected $fillable = ['register', 'person'];
+  protected $fillable = ['register', 'person', 'status_id'];
 
   /**
    * The attributes that should be cast.
@@ -42,7 +42,8 @@ class Providers extends Model
   protected $casts = [
     'id' => 'integer',
     'register' => 'integer',
-    'person' => 'string'
+    'person' => 'string',
+    'status_id' => 'integer'
   ];
 
   /**
@@ -70,5 +71,15 @@ class Providers extends Model
   public function legal(): HasOne
   {
     return $this->hasOne(Legal::class, 'provider_id', 'id');
+  }
+
+  /**
+   * Retrieve the status of the student.
+   *
+   * @return \Illuminate\Database\Eloquent\Relations\HasOne
+   */
+  public function status(): HasOne
+  {
+    return $this->hasOne(StatesNames::class, 'id', 'status_id');
   }
 }
