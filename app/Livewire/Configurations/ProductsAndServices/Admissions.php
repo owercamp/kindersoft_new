ab2c6dc0134028e65fdf30a8441e15d5ec84d466
Ower A. Campos <owerion22@gmail.com>
2024-06-30 07:59:13 -0500

    feat: se crea el componente admisiones

---
+++ app/Livewire/Configurations/ProductsAndServices/Admissions.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace App\Livewire\Configurations\ProductsAndServices;
+
+use Livewire\Component;
+
+class Admissions extends Component
+{
+    public function render()
+    {
+        return view('livewire.configurations.products-and-services.admissions');
+    }
+}
