cf32ba28751eab7b217b487323c5ec98965f1634
Ower A. Campos <owerion22@gmail.com>
2024-06-30 09:31:40 -0500

    refactor: se agregan relaciones

--- app/Models/StatesNames.php
+++ app/Models/StatesNames.php
@@ -5,45 +5,87 @@ namespace App\Models;
 use App\Models\States;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Relations\HasMany;
 use Illuminate\Database\Eloquent\Relations\HasOne;
 
 class StatesNames extends Model
 {
-    use HasFactory;
+  use HasFactory;
+
+  /**
+   * The primary key associated with the table.
+   *
+   * @var string
+   */
+  protected $primaryKey = 'id';
+
+  /**
+   * The attributes that are mass assignable.
+   *
+   * @var array<string>
+   */
+  protected $fillable = ['name'];
+
+  /**
+   * The attributes that should be cast.
+   *
+   * @var array
+   */
+  protected $casts = [
+    'id' => 'integer',
+    'name' => 'string'
+  ];
+
+  /**
+   * Indicates if the model should be timestamped.
+   *
+   * @var bool
+   */
+  public $timestamps = true;
+
+  public function states(): HasOne
+  {
+    return $this->hasOne(States::class, 'state_id', 'id');
+  }
+
+  /**
+   * A description of the entire PHP function.
+   *
+   * @return HasMany
+   */
+  public function attendant(): HasMany
+  {
+    return $this->hasMany(Attendant::class, 'status_id', 'id');
+  }
+
+  /**
+   * A description of the entire PHP function.
+   *
+   * @return HasMany
+   */
+  public function student(): HasMany
+  {
+    return $this->hasMany(Student::class, 'status_id', 'id');
+  }
 
     /**
-     * The primary key associated with the table.
+     * Returns a HasMany relationship between the StatesNames model and the Providers model.
      *
-     * @var string
+     * @return \Illuminate\Database\Eloquent\Relations\HasMany
      */
-    protected $primaryKey = 'id';
+  public function provider(): HasMany
+  {
+    return $this->hasMany(Providers::class, 'status_id', 'id');
+  }
 
-    /**
-     * The attributes that are mass assignable.
-     *
-     * @var array<string>
-     */
-    protected $fillable = ['name'];
-
-    /**
-     * The attributes that should be cast.
-     *
-     * @var array
-     */
-    protected $casts = [
-      'id' => 'integer',
-      'name' => 'string'
-    ];
-
-    /**
-     * Indicates if the model should be timestamped.
-     *
-     * @var bool
-     */
-    public $timestamps = true;
-
-    public function states() : HasOne
-    {
-       return $this->hasOne(States::class, 'state_id', 'id');
-    }
+  /**
+   * Returns a HasMany relationship between the StatesNames model and the 
+   * Admissions model, where the foreign key is 'status_id' and the local key is 'id'.
+   *
+   * @return \Illuminate\Database\Eloquent\Relations\HasMany
+   */
+  public function admission(): HasMany
+  {
+    return $this->hasMany(Admissions::class, 'status_id', 'id');
+  }
 }
