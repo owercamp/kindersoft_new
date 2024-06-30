4311da55fc68e49167864fc67f605d1f52251522
Ower A. Campos <owerion22@gmail.com>
2024-06-30 11:02:53 -0500

    refactor: se agrega valor formateado

--- resources/views/livewire/configurations/Partials/forms-producs-and-services.blade.php
+++ resources/views/livewire/configurations/Partials/forms-producs-and-services.blade.php
@@ -6,8 +6,8 @@
       <x-input-error for="data.register" />
     </div>
     <div class="flex flex-col basis-3/6">
-      <x-label class="ml-1">{{ __('Price') }} {{ __('of') }} {{  $description }}:</x-label>
-      <x-input type="number" min="0" class="border p-2 rounded w-full" wire:model="data.price" id="price" />
+      <x-label class="ml-1">{{ __('Price') }} {{ __('of') }} {{ $description }}: <x-badge>{{ number_format((float)$data['price'], 0, ',', '.') }}</x-badge> </x-label>
+      <x-input type="number" min="0" class="border p-2 rounded w-full" wire:model.live="data.price" id="price" />
       <x-input-error for="data.price" />
     </div>
   </div>
