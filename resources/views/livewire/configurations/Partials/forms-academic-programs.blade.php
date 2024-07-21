cecd590d9d2ff2ea0ceb1b8188426d31502cd26c
Ower A. Campos <owerion22@gmail.com>
2024-07-20 21:36:31 -0500

    feat: se crea el formulario partial

---
+++ resources/views/livewire/configurations/Partials/forms-academic-programs.blade.php
@@ -0,0 +1,14 @@
+<div>
+  <div class="flex flex-row gap-4 mb-4 w-full">
+    <div class="flex flex-col basis-3/6">
+      <x-label class="ml-1">{{ __('Registration') }}:</x-label>
+      <x-input type="text" min="0" maxlength="4" class="border p-2 rounded w-full" wire:model="formGrade.register" id="register" value="{{ isset($formGrade->register) }}" disabled />
+      <x-input-error for="formGrade.register" />
+    </div>
+    <div class="flex flex-col basis-3/6">
+      <x-label class="ml-1">{{ __('Create') }} {{ __('of') }} {{ $description }}:</x-label>
+      <x-input type="text" min="0" maxlength="35" class="border p-2 rounded w-full" wire:model="formGrade.description" id="description" />
+      <x-input-error for="formGrade.description" />
+    </div>
+  </div>
+</div>
