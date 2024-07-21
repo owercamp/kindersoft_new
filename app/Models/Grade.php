88217fa515232eb43fe56de4745defc1222b6bc2
Ower A. Campos <owerion22@gmail.com>
2024-07-20 21:19:15 -0500

    feat: se crea el modelo grado

---
+++ app/Models/Grade.php
@@ -0,0 +1,32 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Grade extends Model
+{
+  use HasFactory;
+
+  /**
+   * The primary key for the model.
+   *
+   * @var string
+   */
+  protected $primaryKey = 'id';
+
+  /**
+   * The attributes that are mass assignable.
+   *
+   * @var array<string>
+   */
+  protected $fillable = ['register', 'description'];
+
+  /**
+   * Indicates if the model should be timestamped.
+   *
+   * @var bool
+   */
+  public $timestamps = true;
+}
