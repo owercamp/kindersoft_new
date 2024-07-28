075ae5feda00f4aa91c3ac3ea99d2817a41d4ca0
Ower A. Campos <owerion22@gmail.com>
2024-07-28 11:46:36 -0500

    feat: se configura modelo periodo

---
+++ app/Models/Period.php
@@ -0,0 +1,71 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Casts\Attribute;
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\HasOne;
+
+class Period extends Model
+{
+  use HasFactory;
+
+  /**
+   * The primary key for the model.
+   *
+   * @var string
+   */
+  protected $primaryKey = 'id';
+
+
+  /**
+   * The attributes that are mass assignable.
+   *
+   * @var array<string>
+   */
+  protected $fillable = ['name', 'register', 'status_id'];
+
+  /**
+   * The attributes that should be cast.
+   *
+   * @var array
+   */
+  protected $casts = [
+    'id' => 'integer',
+    'status_id' => 'integer',
+    'register' => 'integer',
+    'name' => 'string'
+  ];
+
+  /**
+   * Indicates if the model should be timestamped.
+   *
+   * @var bool
+   */
+  public $timestamps = true;
+
+  /**
+   * Interact with the name attribute.
+   *
+   * return \Illuminate\Database\Eloquent\Casts\Attribute
+   */
+  public function name(): Attribute
+  {
+    return Attribute::make(
+      get: fn ($value) => trim($value),
+      set: fn ($value) => trim(ucfirst(strtolower($value))),
+    );
+  }
+
+
+  /**
+   * Retrieve the status associated with this model.
+   *
+   * @return \Illuminate\Database\Eloquent\Relations\HasOne The status relation.
+   */
+  public function status(): HasOne
+  {
+    return $this->hasOne(StatesNames::class, 'id', 'status_id');
+  }
+}
