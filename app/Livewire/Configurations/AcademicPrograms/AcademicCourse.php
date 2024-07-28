f720072939875f6523e5063bc9d9a6a967128532
Ower A. Campos <owerion22@gmail.com>
2024-07-28 07:25:15 -0500

    feat: inicialización del componente cursos

---
+++ app/Livewire/Configurations/AcademicPrograms/AcademicCourse.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace App\Livewire\Configurations\AcademicPrograms;
+
+use Livewire\Component;
+
+class AcademicCourse extends Component
+{
+    public function render()
+    {
+        return view('livewire.configurations.academic-programs.academic-course');
+    }
+}
