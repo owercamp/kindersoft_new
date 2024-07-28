2771d976a58bf395ccce23cd059e5c7448b4f0c4
Ower A. Campos <owerion22@gmail.com>
2024-07-28 07:46:44 -0500

    refactor: se separa caracteristicas comunes

---
+++ app/Service/ConsultingServices.php
@@ -0,0 +1,20 @@
+<?php
+
+namespace App\Service;
+
+use Illuminate\Support\Facades\DB;
+
+class ConsultingServices
+{
+  public static function get_consulting($table, $field): string
+  {
+    $register = DB::table($table)->latest($field)->value($field) + 1;
+    return $register;
+  }
+
+  public static function get_exists($table, $field, $param): bool
+  {
+    $exists = DB::table($table)->where($field, $param)->exists();
+    return $exists;
+  }
+}
