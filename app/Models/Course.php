bc06881a8f40310bf960dc306249239ae95fcb8d
Ower A. Campos <owerion22@gmail.com>
2024-07-28 08:15:30 -0500

    feat: se crea el modelo de cursos y se configura

---
+++ app/Models/Course.php
@@ -0,0 +1,74 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Casts\Attribute;
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\HasOne;
+
+class Course extends Model
+{
+  use HasFactory;
+
+  /**
+   * The primary key for the model.
+   *
+   * @var string
+   */
+  protected $primaryKey = 'id';
+
+  /**
+   * The attributes that are mass assignable.
+   *
+   * @var array
+   */
+  protected $fillable = [
+    'name',
+    'register',
+    'status_id',
+  ];
+
+  /**
+   * The attributes that should be cast.
+   *
+   * @var array
+   */
+  protected $casts = [
+    'id' => 'integer',
+    'name' => 'string',
+    'register' => 'integer',
+    'status_id' => 'integer',
+  ];
+
+  /**
+   * Indicates if the model should be timestamped.
+   *
+   * @var bool
+   */
+  public $timestamps = true;
+
+  /**
+   * Interact with the name attribute.
+   *
+   * return \Illuminate\Database\Eloquent\Casts\Attribute
+   */
+  public function name(): Attribute
+  {
+    return Attribute::make(
+      get: fn ($value) => trim($value),
+      set: fn ($value) => trim(ucfirst(strtolower($value))),
+    );
+  }
+
+
+  /**
+   * Retrieve the status associated with this model.
+   *
+   * @return \Illuminate\Database\Eloquent\Relations\HasOne The status relation.
+   */
+  public function status(): HasOne
+  {
+    return $this->hasOne(StatesNames::class, 'id', 'status_id');
+  }
+}
