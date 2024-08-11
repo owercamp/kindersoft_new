4cb5b5dddf63970ffea9bc235dec89ea4ac030b1
Ower A. Campos <owerion22@gmail.com>
2024-08-11 12:44:30 -0500

    feat: se crea componente achievements

---
+++ app/Livewire/Configurations/ProductsAndServices/Achievements.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace App\Livewire\Configurations\ProductsAndServices;
+
+use Livewire\Component;
+
+class Achievements extends Component
+{
+  public function render()
+  {
+    return view('livewire.configurations.products-and-services.achievements');
+  }
+}
