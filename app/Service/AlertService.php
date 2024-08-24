28356df623eef6844ee30c8762312a46251e5801
Ower A. Campos <owerion22@gmail.com>
2024-08-24 16:34:14 -0500

    refactor: se actualiza la clase alertservices para implementar interfaz notification

--- app/Service/AlertService.php
+++ app/Service/AlertService.php
@@ -2,65 +2,20 @@
 
 namespace App\Service;
 
-class AlertService
+use App\Interfaces\IGetNotification;
+
+class AlertService implements IGetNotification
 {
-  public static function success(): array
+  public static function get_notifications($type, $message, $timer, $status): array
   {
-    $success = [
-      'type' => 'success',
-      'message' => __('Successfully Created Record'),
-      'timer' => 1500,
+    $notified = [
+      'type' => $type,
+      'message' => $message,
+      'timer' => $timer,
       'showConfirmButton' => false,
-      'success' => 'completed'
+      'success' => $status
     ];
-    return $success;
-  }
 
-  public static function warning(): array
-  {
-    $warning = [
-      'type' => 'warning',
-      'message' => __('Record Already Exists'),
-      'timer' => 1500,
-      'showConfirmButton' => false,
-      'success' => 'warning'
-    ];
-    return $warning;
-  }
-
-  public static function error(): array
-  {
-    $error = [
-      'type' => 'error',
-      'message' => __('An error has occurred'),
-      'timer' => 1500,
-      'showConfirmButton' => false,
-      'success' => 'error'
-    ];
-    return $error;
-  }
-
-  public static function info(): array
-  {
-    $info = [
-      'type' => 'info',
-      'message' => __('Successfully Updated Record'),
-      'timer' => 1500,
-      'showConfirmButton' => false,
-      'success' => 'completed'
-    ];
-    return $info;
-  }
-
-  public static function deleted(): array
-  {
-    $deleted = [
-      'type' => 'success',
-      'message' => __('Successfully Deleted Record'),
-      'timer' => 1500,
-      'showConfirmButton' => false,
-      'success' => 'completed'
-    ];
-    return $deleted;
+    return $notified;
   }
 }
