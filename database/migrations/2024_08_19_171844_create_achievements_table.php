35b2c8acaa98224a1aa07d753d8ba73eb77bf9d9
Ower A. Campos <owerion22@gmail.com>
2024-08-25 15:19:18 -0500

    feat: migracion de logros

---
+++ database/migrations/2024_08_19_171844_create_achievements_table.php
@@ -0,0 +1,31 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+  /**
+   * Run the migrations.
+   */
+  public function up(): void
+  {
+    Schema::create('achievements', function (Blueprint $table) {
+      $table->id();
+      $table->foreignId('intelligence_id')->nullable()->constrained('intelligences', 'id')->onUpdate('cascade')->onDelete('set null');
+      $table->unsignedBigInteger('register');
+      $table->string('description');
+      $table->foreignId('status_id')->nullable()->constrained('states_names', 'id')->onUpdate('cascade')->onDelete('set null');
+      $table->timestamps();
+    });
+  }
+
+  /**
+   * Reverse the migrations.
+   */
+  public function down(): void
+  {
+    Schema::dropIfExists('achievements');
+  }
+};
