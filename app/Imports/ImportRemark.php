d933e4a3953456415018209c7d3d278552ed34b6
Ower A. Campos <owerion22@gmail.com>
2024-10-05 07:20:27 -0500

    feat: se crean los importes desde archivo de excel closed #67

---
+++ app/Imports/ImportRemark.php
@@ -0,0 +1,37 @@
+<?php
+
+namespace App\Imports;
+
+use App\Models\Intelligence;
+use App\Models\Remark;
+use Illuminate\Support\Collection;
+use Maatwebsite\Excel\Concerns\ToCollection;
+
+class ImportRemark implements ToCollection
+{
+  public function collection(Collection $rows)
+  {
+    foreach ($rows as $row) {
+      if ($row[0] != "Inteligencia" && $row[0]) {
+        $my_intelligence = self::getIntelligence($row[0]);
+        $intelligence_id = $my_intelligence->id;
+        $increment = $my_intelligence->register;
+        $description = $row[1];
+
+        if ($intelligence_id) {
+          $register = new Remark();
+          $register->intelligence_id = $intelligence_id;
+          $register->register = $increment;
+          $register->description = $description;
+          $register->save();
+        }
+      }
+    }
+  }
+
+  private function getIntelligence($data)
+  {
+    $intelligence = Intelligence::where('name', $data)->select('id', 'register')->first();
+    return $intelligence;
+  }
+}
