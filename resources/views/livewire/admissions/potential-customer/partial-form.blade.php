c1dabd193b31c49b5f462b84644c5090c3effe99
Ower A. Campos <owerion22@gmail.com>
2024-10-05 13:55:10 -0500

    creacion de formulario de cliente potencial

---
+++ resources/views/livewire/admissions/potential-customer/partial-form.blade.php
@@ -0,0 +1,69 @@
+<div>
+  <div class="flex flex-row gap-4 mb-4 w-full">
+    <div class="max-w-[9rem]">
+      <x-label>{{ __('Registration') }}</x-label>
+      <x-input wire:model="registerForm.register" disabled type="text" class="mt-1 block w-full" />
+      <x-input-error for="registerForm.register" />
+    </div>
+    <div class="min-w-[10rem]">
+      <x-label style="text-transform: capitalize;">{{ __('validation.attributes.date') }}</x-label>
+      <x-input type="date" class="mt-1" wire:model="registerForm.date" />
+      <x-input-error for="registerForm.date" />
+    </div>
+    <div class="w-full">
+      <x-label>{{ __('Name') }} {{ rtrim(__('Attendees'), 's') }}</x-label>
+      <x-input type="text" class="mt-1 block w-full" wire:model="registerForm.name" />
+      <x-input-error for="registerForm.name" />
+    </div>
+  </div>
+  <div class="flex flex-row gap-4 mb-4 w-full">
+    <div class="max-w-[10rem]">
+      <x-label>{{ __('Phone') }} {{ ucfirst(__('validation.attributes.mobile')) }}</x-label>
+      <x-input type="text" class="mt-1 block w-full" wire:model="registerForm.phone" />
+      <x-input-error for="registerForm.phone" />
+    </div>
+    <div class="max-w-[10rem]">
+      <x-label> WhatsApp</x-label>
+      <x-input type="text" class="mt-1 block w-full" wire:model="registerForm.whatsapp" />
+      <x-input-error for="registerForm.whatsapp" />
+    </div>
+    <div class="w-full">
+      <x-label>{{ __('Email') }}</x-label>
+      <x-input type="email" class="mt-1 block w-full" wire:model="registerForm.email" />
+      <x-input-error for="registerForm.email" />
+    </div>
+  </div>
+  <div class="flex flex-row gap-4 mb-4 w-full">
+    <div class="max-w-[9rem]">
+      <x-label>{{ __('Number of applicants') }}</x-label>
+      <x-input type="number" class="mt-1 block w-full" wire:model="registerForm.applicants" />
+      <x-input-error for="registerForm.applicants" />
+    </div>
+    <div class="w-full">
+      <x-label>{{ __('Name') }} {{ __('Applicant') }}</x-label>
+      <x-input type="text" class="mt-1 block w-full" wire:model="registerForm.applicant" />
+      <x-input-error for="registerForm.applicant" />
+    </div>
+  </div>
+  <div class="flex flex-row gap-4 mb-4 w-full">
+    <div class="max-w-[15rem]">
+      <x-label>{{ __('Genre') }}</x-label>
+      <x-select-options class="mt-1" title="genres" wire:model="registerForm.genre">
+        <option value="">{{ __('Select') }}...</option>
+        @foreach ($genres as $genre)
+        <option value="{{ $genre }}">{{ $genre }}</option>
+        @endforeach
+      </x-select-options>
+      <x-input-error for="registerForm.genre" />
+    </div>
+    <div class="max-w-[10rem]">
+      <x-label>{{ ucfirst(__('validation.attributes.date_of_birth')) }}</x-label>
+      <x-input type="date" class="mt-1" wire:model="registerForm.birthday" wire:change="calculateAge" />
+      <x-input-error for="registerForm.birthday" />
+    </div>
+    <div class="w-full">
+      <x-label>{{ __('Age') }}</x-label>
+      <x-badge-secondary class="mt-3">{{ $age }}</x-badge-secondary>
+    </div>
+  </div>
+</div>
