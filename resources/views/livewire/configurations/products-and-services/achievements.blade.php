d71fa8a56c0c519e4f8f9a9cacef8860945a09a3
Ower A. Campos <owerion22@gmail.com>
2024-10-05 07:14:10 -0500

    refactor: se actualiza el formulario de logros

--- resources/views/livewire/configurations/products-and-services/achievements.blade.php
+++ resources/views/livewire/configurations/products-and-services/achievements.blade.php
@@ -1,7 +1,7 @@
 <div>
   <x-slot name="header">
     <h2 class="font-semibold text-xl text-gray-800 dark:text-gray-200 leading-tight text-end px-4">
-      {{ __('Configuration') }} > {{ __('Products and Services') }} > {{ __('Achievements') }}
+      {{ __('Configuration') }} > {{ __('Academic Programs') }} > {{ __('Achievements') }}
     </h2>
   </x-slot>
   <div class="py-8">
