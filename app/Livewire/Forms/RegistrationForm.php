bf013f400c9d1b131cb5142c109f4f9cd4b9e586
Ower A. Campos <owerion22@gmail.com>
2024-10-05 12:23:40 -0500

    feat: se crea el formulario de cliente potencial

---
+++ app/Livewire/Forms/RegistrationForm.php
@@ -0,0 +1,52 @@
+<?php
+
+namespace App\Livewire\Forms;
+
+use Livewire\Attributes\Validate;
+use Livewire\Form;
+
+class RegistrationForm extends Form
+{
+  public string $register = '';
+  public string $date = '';
+  public string $name = '';
+  public string $phone = '';
+  public string $whatsapp = '';
+  public string $email = '';
+  public string $applicants = '';
+  public string $applicant = '';
+  public int $genre = 0;
+
+  public function rules()
+  {
+    return [
+      'register' => 'required|numeric',
+      'date' => 'required|date',
+      'name' => 'required|string',
+      'phone' => 'required|numeric',
+      'email' => 'required|email',
+      'applicants' => 'required|numeric',
+      'applicant' => 'required|string',
+      'genre' => 'required|numeric|exists:genres,id'
+    ];
+  }
+
+  public function messages()
+  {
+    return [];
+  }
+
+  public function validationAttributes()
+  {
+    return [
+      'register' => __('Registration'),
+      'date' => ucfirst(__('validation.attributes.date')),
+      'name' => __('Name'),
+      'phone' => __('Phone'),
+      'email' => __('Email address'),
+      'applicants' => __('Number of applicants'),
+      'applicant' => __('Name') . ' ' . __('Applicant'),
+      'genre' => __('Genre')
+    ];
+  }
+}
