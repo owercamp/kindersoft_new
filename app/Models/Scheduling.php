4e258ccda4a347cdd9fe0fe0d52707e6f68aaf6a
Ower A. Campos <owerion22@gmail.com>
2024-10-19 14:50:04 -0500

    feat: se agrega formulario para agendamiento

---
+++ app/Models/Scheduling.php
@@ -0,0 +1,43 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Scheduling extends Model
+{
+  use HasFactory;
+
+  /**
+   * The primary key for the model.
+   *
+   * @var string
+   */
+  protected $primaryKey = 'id';
+
+  /**
+   * The attributes that are mass assignable.
+   *
+   * @var array<string>
+   */
+  protected $fillable = ['date', 'time', 'potential_customer_id'];
+
+  /**
+   * The attributes that should be cast.
+   *
+   * @var array
+   */
+  protected $casts = [
+    'date' => 'date',
+    'time' => 'time',
+    'potential_customer_id' => 'integer',
+  ];
+
+  /**
+   * Indicates if the model should be timestamped.
+   *
+   * @var bool
+   */
+  public $timestamps = true;
+}
