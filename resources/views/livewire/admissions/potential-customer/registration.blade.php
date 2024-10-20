58172087e8516ece36698c796273ae18a6a6aa6a
Ower A. Campos <owerion22@gmail.com>
2024-10-20 14:40:53 -0500

    chore: se cambia la visualización de los registros no agendados

--- resources/views/livewire/admissions/potential-customer/registration.blade.php
+++ resources/views/livewire/admissions/potential-customer/registration.blade.php
@@ -133,66 +133,68 @@
               </thead>
               <tbody>
                 @foreach ($registers as $potential)
-                <tr class="bg-white border-b dark:bg-gray-800 dark:border-gray-700 hover:bg-gray-50 dark:hover:bg-gray-600" wire:key="doc-{{ $potential->id }}">
-                  <td scope="row" class="text-center px-6 py-4 font-medium text-gray-900 whitespace-nowrap dark:text-white">
-                    {{ str_pad($potential->register, 4, '0', STR_PAD_LEFT) }}
-                  </td>
-                  <td class="text-center">
-                    {{ ucfirst($potential->name_attendant) }}
-                  </td>
-                  <td class="text-center">
-                    {{ $potential->phone }} - {{ $potential->whatsapp }}
-                  </td>
-                  <td class="text-center">
-                    {{ __($potential->name_applicant) }}
-                  </td>
-                  <td scope="row" class="flex justify-around justify-items-center">
-                    <x-button class="bg-green-800 hover:bg-green-700 my-2 flex flex-row" wire:click="openModal({{ $potential->id }})" wire:loading.class="opacity-50" wire:loading.attr="disabled">
-                      <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="w-4 h-4 mr-1">
-                        <path stroke-linecap="round" stroke-linejoin="round" d="m16.862 4.487 1.687-1.688a1.875 1.875 0 1 1 2.652 2.652L10.582 16.07a4.5 4.5 0 0 1-1.897 1.13L6 18l.8-2.685a4.5 4.5 0 0 1 1.13-1.897l8.932-8.931Zm0 0L19.5 7.125M18 14v4.75A2.25 2.25 0 0 1 15.75 21H5.25A2.25 2.25 0 0 1 3 18.75V8.25A2.25 2.25 0 0 1 5.25 6H10" />
-                      </svg>
-                      <div wire:loading.class="hidden" wire:target="openModal({{ $potential->id }})">
-                        {{ __('Edit') }}
-                      </div>
-                      <div wire:loading wire:target="openModal({{ $potential->id }})">
-                        {{ __('Consulting...') }}
-                      </div>
-                    </x-button>
-                    <x-button class="bg-red-800 hover:bg-red-700 my-2 flex flex-row" wire:click="delete({{ $potential->id }})">
-                      <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="w-4 h-4 mr-1">
-                        <path stroke-linecap="round" stroke-linejoin="round" d="m14.74 9-.346 9m-4.788 0L9.26 9m9.968-3.21c.342.052.682.107 1.022.166m-1.022-.165L18.16 19.673a2.25 2.25 0 0 1-2.244 2.077H8.084a2.25 2.25 0 0 1-2.244-2.077L4.772 5.79m14.456 0a48.108 48.108 0 0 0-3.478-.397m-12 .562c.34-.059.68-.114 1.022-.165m0 0a48.11 48.11 0 0 1 3.478-.397m7.5 0v-.916c0-1.18-.91-2.164-2.09-2.201a51.964 51.964 0 0 0-3.32 0c-1.18.037-2.09 1.022-2.09 2.201v.916m7.5 0a48.667 48.667 0 0 0-7.5 0" />
-                      </svg>
-                      <div wire:loading.class="hidden" wire:target="delete({{ $potential->id }})">
-                        {{ __('Delete') }}
-                      </div>
-                      <div wire:loading wire:target="delete({{ $potential->id }})">
-                        {{ __('Removing...') }}
-                      </div>
-                    </x-button>
-                    <x-button class="bg-sky-800 hover:bg-sky-700 my-2 flex flex-row" wire:click="export({{ $potential->id }})">
-                      <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="w-4 h-4 mr-1">
-                        <path fill-rule="evenodd" d="M12 2.25a.75.75 0 0 1 .75.75v11.69l3.22-3.22a.75.75 0 1 1 1.06 1.06l-4.5 4.5a.75.75 0 0 1-1.06 0l-4.5-4.5a.75.75 0 1 1 1.06-1.06l3.22 3.22V3a.75.75 0 0 1 .75-.75Zm-9 13.5a.75.75 0 0 1 .75.75v2.25a1.5 1.5 0 0 0 1.5 1.5h13.5a1.5 1.5 0 0 0 1.5-1.5V16.5a.75.75 0 0 1 1.5 0v2.25a3 3 0 0 1-3 3H5.25a3 3 0 0 1-3-3V16.5a.75.75 0 0 1 .75-.75Z" clip-rule="evenodd" />
-                      </svg>
-                      <div wire:loading.class="hidden" wire:target="export({{ $potential->id }})">
-                        {{ __('actions.export') }}
-                      </div>
-                      <div wire:loading wire:target="export({{ $potential->id }})">
-                        {{ __('Exporting...') }}
-                      </div>
-                    </x-button>
-                    <x-button class="bg-yellow-800 hover:bg-yellow-700 my-2 flex flex-row" wire:click="schedule_model({{ $potential->id }})">
-                      <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="w-4 h-4 mr-1">
-                        <path fill-rule="evenodd" d="M2.625 6.75a1.125 1.125 0 1 1 2.25 0 1.125 1.125 0 0 1-2.25 0Zm4.875 0A.75.75 0 0 1 8.25 6h12a.75.75 0 0 1 0 1.5h-12a.75.75 0 0 1-.75-.75ZM2.625 12a1.125 1.125 0 1 1 2.25 0 1.125 1.125 0 0 1-2.25 0ZM7.5 12a.75.75 0 0 1 .75-.75h12a.75.75 0 0 1 0 1.5h-12A.75.75 0 0 1 7.5 12Zm-4.875 5.25a1.125 1.125 0 1 1 2.25 0 1.125 1.125 0 0 1-2.25 0Zm4.875 0a.75.75 0 0 1 .75-.75h12a.75.75 0 0 1 0 1.5h-12a.75.75 0 0 1-.75-.75Z" clip-rule="evenodd" />
-                      </svg>
-                      <div wire:loading.class="hidden" wire:target="schedule_model({{ $potential->id }})">
-                        {{ __('Schedule') }}
-                      </div>
-                      <div wire:loading wire:target="schedule_model({{ $potential->id }})">
-                        {{ __('Scheduling') }}
-                      </div>
-                    </x-button>
-                  </td>
-                </tr>
+                  @if (!isset($potential->asigned))
+                    <tr class="bg-white border-b dark:bg-gray-800 dark:border-gray-700 hover:bg-gray-50 dark:hover:bg-gray-600" wire:key="doc-{{ $potential->id }}">
+                      <td scope="row" class="text-center px-6 py-4 font-medium text-gray-900 whitespace-nowrap dark:text-white">
+                        {{ str_pad($potential->register, 4, '0', STR_PAD_LEFT) }}
+                      </td>
+                      <td class="text-center">
+                        {{ ucfirst($potential->name_attendant) }}
+                      </td>
+                      <td class="text-center">
+                        {{ $potential->phone }} - {{ $potential->whatsapp }}
+                      </td>
+                      <td class="text-center">
+                        {{ __($potential->name_applicant) }}
+                      </td>
+                      <td scope="row" class="flex justify-around justify-items-center">
+                        <x-button class="bg-green-800 hover:bg-green-700 my-2 flex flex-row" wire:click="openModal({{ $potential->id }})" wire:loading.class="opacity-50" wire:loading.attr="disabled">
+                          <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="w-4 h-4 mr-1">
+                            <path stroke-linecap="round" stroke-linejoin="round" d="m16.862 4.487 1.687-1.688a1.875 1.875 0 1 1 2.652 2.652L10.582 16.07a4.5 4.5 0 0 1-1.897 1.13L6 18l.8-2.685a4.5 4.5 0 0 1 1.13-1.897l8.932-8.931Zm0 0L19.5 7.125M18 14v4.75A2.25 2.25 0 0 1 15.75 21H5.25A2.25 2.25 0 0 1 3 18.75V8.25A2.25 2.25 0 0 1 5.25 6H10" />
+                          </svg>
+                          <div wire:loading.class="hidden" wire:target="openModal({{ $potential->id }})">
+                            {{ __('Edit') }}
+                          </div>
+                          <div wire:loading wire:target="openModal({{ $potential->id }})">
+                            {{ __('Consulting...') }}
+                          </div>
+                        </x-button>
+                        <x-button class="bg-red-800 hover:bg-red-700 my-2 flex flex-row" wire:click="delete({{ $potential->id }})">
+                          <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="w-4 h-4 mr-1">
+                            <path stroke-linecap="round" stroke-linejoin="round" d="m14.74 9-.346 9m-4.788 0L9.26 9m9.968-3.21c.342.052.682.107 1.022.166m-1.022-.165L18.16 19.673a2.25 2.25 0 0 1-2.244 2.077H8.084a2.25 2.25 0 0 1-2.244-2.077L4.772 5.79m14.456 0a48.108 48.108 0 0 0-3.478-.397m-12 .562c.34-.059.68-.114 1.022-.165m0 0a48.11 48.11 0 0 1 3.478-.397m7.5 0v-.916c0-1.18-.91-2.164-2.09-2.201a51.964 51.964 0 0 0-3.32 0c-1.18.037-2.09 1.022-2.09 2.201v.916m7.5 0a48.667 48.667 0 0 0-7.5 0" />
+                          </svg>
+                          <div wire:loading.class="hidden" wire:target="delete({{ $potential->id }})">
+                            {{ __('Delete') }}
+                          </div>
+                          <div wire:loading wire:target="delete({{ $potential->id }})">
+                            {{ __('Removing...') }}
+                          </div>
+                        </x-button>
+                        <x-button class="bg-sky-800 hover:bg-sky-700 my-2 flex flex-row" wire:click="export({{ $potential->id }})">
+                          <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="w-4 h-4 mr-1">
+                            <path fill-rule="evenodd" d="M12 2.25a.75.75 0 0 1 .75.75v11.69l3.22-3.22a.75.75 0 1 1 1.06 1.06l-4.5 4.5a.75.75 0 0 1-1.06 0l-4.5-4.5a.75.75 0 1 1 1.06-1.06l3.22 3.22V3a.75.75 0 0 1 .75-.75Zm-9 13.5a.75.75 0 0 1 .75.75v2.25a1.5 1.5 0 0 0 1.5 1.5h13.5a1.5 1.5 0 0 0 1.5-1.5V16.5a.75.75 0 0 1 1.5 0v2.25a3 3 0 0 1-3 3H5.25a3 3 0 0 1-3-3V16.5a.75.75 0 0 1 .75-.75Z" clip-rule="evenodd" />
+                          </svg>
+                          <div wire:loading.class="hidden" wire:target="export({{ $potential->id }})">
+                            {{ __('actions.export') }}
+                          </div>
+                          <div wire:loading wire:target="export({{ $potential->id }})">
+                            {{ __('Exporting...') }}
+                          </div>
+                        </x-button>
+                        <x-button class="bg-yellow-800 hover:bg-yellow-700 my-2 flex flex-row" wire:click="schedule_model({{ $potential->id }})">
+                          <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="w-4 h-4 mr-1">
+                            <path fill-rule="evenodd" d="M2.625 6.75a1.125 1.125 0 1 1 2.25 0 1.125 1.125 0 0 1-2.25 0Zm4.875 0A.75.75 0 0 1 8.25 6h12a.75.75 0 0 1 0 1.5h-12a.75.75 0 0 1-.75-.75ZM2.625 12a1.125 1.125 0 1 1 2.25 0 1.125 1.125 0 0 1-2.25 0ZM7.5 12a.75.75 0 0 1 .75-.75h12a.75.75 0 0 1 0 1.5h-12A.75.75 0 0 1 7.5 12Zm-4.875 5.25a1.125 1.125 0 1 1 2.25 0 1.125 1.125 0 0 1-2.25 0Zm4.875 0a.75.75 0 0 1 .75-.75h12a.75.75 0 0 1 0 1.5h-12a.75.75 0 0 1-.75-.75Z" clip-rule="evenodd" />
+                          </svg>
+                          <div wire:loading.class="hidden" wire:target="schedule_model({{ $potential->id }})">
+                            {{ __('Schedule') }}
+                          </div>
+                          <div wire:loading wire:target="schedule_model({{ $potential->id }})">
+                            {{ __('Scheduling') }}
+                          </div>
+                        </x-button>
+                      </td>
+                    </tr>
+                  @endif
                 @endforeach
               </tbody>
             </table>
