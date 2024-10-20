2d53d05c14de4e07a533ca228208b5766f5b64b6
Ower A. Campos <owerion22@gmail.com>
2024-10-20 14:26:07 -0500

    feat: se agrega relacion con potencioal customer

--- app/Models/Scheduling.php
+++ app/Models/Scheduling.php
@@ -2,8 +2,10 @@
 
 namespace App\Models;
 
+use App\Models\PotentialCustomer;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\HasOne;
 
 class Scheduling extends Model
 {
@@ -40,4 +42,14 @@ class Scheduling extends Model
    * @var bool
    */
   public $timestamps = true;
+
+  /**
+   * Get the PotentialCustomer record associated with the Scheduling.
+   *
+   * @return \Illuminate\Database\Eloquent\Relations\HasOne
+   */
+  public function customer_client(): HasOne
+  {
+    return $this->hasOne(PotentialCustomer::class, 'id', 'potential_customer_id');
+  }
 }
