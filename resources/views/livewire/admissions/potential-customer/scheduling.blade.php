43dd2e3116132be1b0f8f86bce88cc308ef5d30d
Ower A. Campos <owerion22@gmail.com>
2024-10-25 16:43:46 -0500

    chore: se cambia color a boton ver

--- resources/views/livewire/admissions/potential-customer/scheduling.blade.php
+++ resources/views/livewire/admissions/potential-customer/scheduling.blade.php
@@ -119,7 +119,7 @@
                     {{ $potential->customer_client->whatsapp }}
                   </td>
                   <td scope="row" class="flex justify-around justify-items-center">
-                    <x-button class="bg-green-800 hover:bg-green-700 my-2 flex flex-row" wire:click="openModal({{ $potential->id }})" wire:loading.class="opacity-50" wire:loading.attr="disabled">
+                    <x-button class="bg-stone-800 hover:bg-stone-700 my-2 flex flex-row" wire:click="openModal({{ $potential->id }})" wire:loading.class="opacity-50" wire:loading.attr="disabled">
                       <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="currentColor" class="w-4 h-4 mr-1">
                         <path d="M12 15a3 3 0 1 0 0-6 3 3 0 0 0 0 6Z" />
                         <path fill-rule="evenodd" d="M1.323 11.447C2.811 6.976 7.028 3.75 12.001 3.75c4.97 0 9.185 3.223 10.675 7.69.12.362.12.752 0 1.113-1.487 4.471-5.705 7.697-10.677 7.697-4.97 0-9.186-3.223-10.675-7.69a1.762 1.762 0 0 1 0-1.113ZM17.25 12a5.25 5.25 0 1 1-10.5 0 5.25 5.25 0 0 1 10.5 0Z" clip-rule="evenodd" />
