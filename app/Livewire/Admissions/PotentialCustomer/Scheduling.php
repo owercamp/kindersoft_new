240c67a4d17b159ff799342b795cf7b0c726ac91
Ower A. Campos <owerion22@gmail.com>
2024-10-25 16:39:37 -0500

    feat: se agrega apertuta del modal de edicion

--- app/Livewire/Admissions/PotentialCustomer/Scheduling.php
+++ app/Livewire/Admissions/PotentialCustomer/Scheduling.php
@@ -2,7 +2,9 @@
 
 namespace App\Livewire\Admissions\PotentialCustomer;
 
+use App\Livewire\Forms\scheduleForm;
 use App\Service\SchedulingService;
+use Carbon\Carbon;
 use Livewire\Component;
 use Livewire\WithPagination;
 
@@ -11,6 +13,8 @@ class Scheduling extends Component
   use WithPagination;
 
   public bool $modal = false;
+  public bool $edition = false;
+  public scheduleForm $scheduleForm;
   public object $info;
 
   public function openModal(int $id)
@@ -19,6 +23,15 @@ class Scheduling extends Component
     $this->info = $data;
     $this->modal = true;
   }
+
+  public function openEdit(int $id)
+  {
+    $data = SchedulingService::show($id);
+    $this->scheduleForm->date = Carbon::parse($data->date)->format('Y-m-d');
+    $this->scheduleForm->time = $data->time;
+    $this->edition = true;
+  }
+
   public function render()
   {
     $registers = SchedulingService::all();
