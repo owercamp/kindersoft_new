116b5aa4e18c681908ec113f1e1229bf5462dbfb
Ower A. Campos <owerion22@gmail.com>
2024-10-25 17:23:47 -0500

    feat: se implementa funcion edit

--- app/Service/SchedulingService.php
+++ app/Service/SchedulingService.php
@@ -2,15 +2,32 @@
 
 namespace App\Service;
 
+use App\Livewire\Forms\scheduleForm;
 use App\Models\Scheduling;
+use App\Service\ConsultingServices;
+use App\Service\Notified\SuccessNotification;
+use Carbon\Carbon;
 
-class SchedulingService
+class SchedulingService extends ConsultingServices
 {
 
   public static function all()
   {
     $registers = Scheduling::with('customer_client')->paginate(10);
     return $registers;
+  }
+
+  public static function edit(scheduleForm $schedule, int $id)
+  {
+    $register = self::get_exists('schedulings', ['id' => $id]);
+    if ($register) {
+      $new_schedule = Scheduling::find($id);
+      $new_schedule->date = $schedule->date;
+      $new_schedule->time = Carbon::parse($schedule->time)->format('H:i');
+      if ($new_schedule->save()) {
+        return SuccessNotification::get_notifications('success', __('Successfully Updated Record'), 1500, 'completed');
+      }
+    }
   }
 
   public static function show(int $id): object
