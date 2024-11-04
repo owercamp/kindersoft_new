281af2ba3eff406406586cba168cc9b9f3d218d9
Ower A. Campos <owerion22@gmail.com>
2024-11-04 14:15:09 -0500

    chore: se cambia llamado a funcion all por all_ordered

--- app/Livewire/Admissions/PotentialCustomer/Archive.php
+++ app/Livewire/Admissions/PotentialCustomer/Archive.php
@@ -7,9 +7,20 @@ use Livewire\Component;
 
 class Archive extends Component
 {
+  public bool $modal = false;
+  public object $info;
+
+  public function openModal(int $id): void
+  {
+    $register = SchedulingService::show($id);
+    $this->info = $register;
+    $this->modal = true;
+  }
+
   public function render()
   {
-    $registers = SchedulingService::all();
+    $registers = SchedulingService::all_ordered();
+    // dd($registers);
     return view('livewire.admissions.potential-customer.archive', compact('registers'));
   }
 }
