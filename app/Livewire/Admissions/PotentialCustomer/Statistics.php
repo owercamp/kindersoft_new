8e182a05a632142d32e56c4fb80e6f70beaf0d84
Ower A. Campos <owerion22@gmail.com>
2024-11-06 17:29:30 -0500

    feat: creacion de componente

---
+++ app/Livewire/Admissions/PotentialCustomer/Statistics.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace App\Livewire\Admissions\PotentialCustomer;
+
+use Livewire\Component;
+
+class Statistics extends Component
+{
+    public function render()
+    {
+        return view('livewire.admissions.potential-customer.statistics');
+    }
+}
