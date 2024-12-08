65f8f61168358ff5fba79d971157a19b4964257b
Ower A. Campos <owerion22@gmail.com>
2024-12-07 21:26:27 -0500

    refactor: se configura la inplementación de grafica

--- app/Livewire/Admissions/PotentialCustomer/Statistics.php
+++ app/Livewire/Admissions/PotentialCustomer/Statistics.php
@@ -2,12 +2,40 @@
 
 namespace App\Livewire\Admissions\PotentialCustomer;
 
+use App\Service\StadisticService;
+use Carbon\Carbon;
 use Livewire\Component;
 
 class Statistics extends Component
 {
-    public function render()
-    {
-        return view('livewire.admissions.potential-customer.statistics');
+  public $chartData;
+  public $categories;
+
+  public function mount()
+  {
+    $year = Carbon::now()->year;
+    $min_age = $year - 5;
+
+    $information = StadisticService::data_year($year, $min_age);
+
+    $series = [];
+
+    foreach ($information as $info) {
+      $series[] = $info['attended'];
+      $series[] = $info['not_attended'];
     }
+
+    $this->chartData = [
+      'series' => $series,
+    ];
+
+    $this->categories = [
+      'categories' => ['Enero', 'Febrero', 'Marzo', 'Abril', 'Mayo', 'Junio', 'Julio', 'Agosto', 'Septiembre', 'Octubre', 'Noviembre', 'Diciembre']
+    ];
+
+  }
+  public function render()
+  {
+    return view('livewire.admissions.potential-customer.statistics');
+  }
 }
