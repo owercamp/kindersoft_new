5090d67b1461fc16989422f8129335e7e5a82234
Ower A. Campos <owerion22@gmail.com>
2024-12-11 08:59:35 -0500

    refactor: se modifica el componente de boton

--- resources/views/components/button.blade.php
+++ resources/views/components/button.blade.php
@@ -1,3 +1,9 @@
-<button {{ $attributes->merge(['type' => 'submit', 'class' => 'items-center px-4 py-2 border border-transparent rounded-md font-semibold text-xs text-white uppercase tracking-widest focus:outline-none focus:ring-2 focus:ring-indigo-500 focus:ring-offset-2 dark:focus:ring-offset-gray-800 transition ease-in-out duration-150']) }}>
+@props(['px' => null])
+
+@php
+  $px = $px ?? 'px-4';
+@endphp
+
+<button {{ $attributes->merge(["type" => "submit", "class" => "items-center {$px} py-2 border border-transparent rounded-md font-semibold text-xs text-white uppercase tracking-widest focus:outline-none focus:ring-2 focus:ring-indigo-500 focus:ring-offset-2 dark:focus:ring-offset-gray-800 transition ease-in-out duration-150"]) }}>
     {{ $slot }}
 </button>
