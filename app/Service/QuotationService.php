0860b7168622a8d1c15ad6a207a2284d68fa9e40
Ower A. Campos <owerion22@gmail.com>
2025-01-03 14:15:13 -0500

    chore: cambios para refactorizar

--- app/Service/QuotationService.php
+++ app/Service/QuotationService.php
@@ -3,7 +3,15 @@
 namespace App\Service;
 
 use App\Livewire\Forms\QuoteForm;
+use App\Models\Admissions;
+use App\Models\Extracurricular;
+use App\Models\ExtraTime;
+use App\Models\Feeding;
+use App\Models\Journays;
 use App\Models\Quotation;
+use App\Models\Quotationable;
+use App\Models\Transport;
+use App\Models\Uniform;
 use App\Service\ConsultingServices;
 use App\Service\Notified\ErrorNotification;
 use App\Service\Notified\SuccessNotification;
@@ -11,57 +19,70 @@ use Illuminate\Support\Facades\DB;
 
 class QuotationService extends ConsultingServices
 {
-  public static function create(QuoteForm $form, int $id)
+  public static function create(QuoteForm $form, int $id, array $data)
   {
     $register = new Quotation();
     $register->register = $form->register;
     $register->date = $form->date;
     $register->scheduling_id = $id;
-
-    if ($form->admissions) {
-      $register->admission_id = $form->admissions;
-    }
-
-    if ($form->journal) {
-      $register->journal_id = $form->journal;
-    }
-
-    if ($form->food) {
-      $register->feeding_id = $form->food;
-    }
-
-    if ($form->uniform) {
-      $register->uniform_id = $form->uniform;
-    }
-
-    if ($form->add_time) {
-      $register->extra_time_id = $form->add_time;
-    }
-
-    if ($form->extracurricular) {
-      $register->extra_curricular_id = $form->extracurricular;
-    }
-
-    if ($form->transport) {
-      $register->transport_id = $form->transport;
-    }
-
     if ($register->save()) {
+      foreach ($data as $key => $value) {
+        $activity = new Quotationable();
+        $activity->quotation_id = $register->id;
+        $activity->quotationable_id = $value['id'];
+        $activity->quotationable_type = $value['table'];
+        $activity->save();
+      }
       return SuccessNotification::get_notifications('success', __('Successfully Created Record'), 1500, 'completed');
     } else {
       return ErrorNotification::get_notifications('error', __('Error Creating Record'), 1500, 'error');
     }
   }
 
-  public static function get_consulting($table, $params): object
+  public static function get_consulting_array($table, $params): array
   {
-    $information = DB::table($table)->where($params)->select('description', 'price')->get();
-    return $information;
+    $array_information = [];
+
+    if ($table == 'admissions') {
+      $model = Admissions::class;
+    }
+
+    if ($table == 'journays') {
+      $model = Journays::class;
+    }
+
+    if ($table == 'feedings') {
+      $model = Feeding::class;
+    }
+
+    if ($table == 'uniforms') {
+      $model = Uniform::class;
+    }
+
+    if ($table == 'extra_times') {
+      $model = ExtraTime::class;
+    }
+
+    if ($table == 'extracurriculars') {
+      $model = Extracurricular::class;
+    }
+
+    if ($table == 'transports') {
+      $model = Transport::class;
+    }
+
+    $information = DB::table($table)->where($params)->select('description', 'price', 'id')->get()->toArray();
+
+    $array_information['description'] = $information[0]->description;
+    $array_information['price'] = $information[0]->price;
+    $array_information['id'] = $information[0]->id;
+    $array_information['table'] = $model;
+    return $array_information;
   }
 
   public static function show()
   {
-    $registers = Quotation::with('scheduling','scheduling.customer_client', 'admission', 'journal', 'feeding', 'uniform', 'extra_time', 'extra_curricular', 'transport')->paginate(10);
+    $registers = Quotation::with('scheduling', 'scheduling.customer_client')->paginate(10);
     return $registers;
   }
 }
