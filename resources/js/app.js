232861c6646df75547fa249543f32b95e8c8d5e8
Ower A. Campos <owerion22@gmail.com>
2025-08-06 07:37:29 -0500

    feat(persist): implement persist alpine

--- resources/js/app.js
+++ resources/js/app.js
@@ -2,11 +2,15 @@ import './bootstrap';
 
 // import Alpine from 'alpinejs';
 import focus from '@alpinejs/focus';
+import persist from '@alpinejs/persist';
 import Swal from 'sweetalert2';
 import './main';
+import './text-rich';
+import Alpine from 'alpinejs';
 // window.Alpine = Alpine;
 window.Swal = Swal;
 
 Alpine.plugin(focus);
+Alpine.plugin(persist);
 // Alpine.start();
 
