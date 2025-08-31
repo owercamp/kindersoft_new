76f16f51cd95ea31bc86dcae895c1095382a5130
Ower A. Campos <owerion22@gmail.com>
2025-08-31 18:29:35 -0500

    feat(service): :sparkles: create services for admissions

---
+++ app/Service/AdmissionService.php
@@ -0,0 +1,99 @@
+<?php
+
+namespace App\Service;
+
+use App\Models\Notification;
+use App\Service\SanitizeService;
+use Illuminate\Support\Facades\DB;
+use App\Service\ConsultingServices;
+use App\Service\Uploads\UploadService;
+use App\Livewire\Forms\NotificationForm;
+use App\Service\Notified\InfoNotification;
+use App\Service\Notified\ErrorNotification;
+use App\Service\Notified\SuccessNotification;
+
+class AdmissionService
+{
+  static function saveAdmission(NotificationForm $form)
+  {
+    $path = null;
+    DB::beginTransaction();
+    try {
+      if ($form->firm) {
+        $path = UploadService::upload('Firmas', $form->firm);
+      }
+
+      $email = SanitizeService::sanitize($form->email, 'email');
+      $content = SanitizeService::sanitize($form->content, 'html');
+
+      $administrative = new Notification();
+      $administrative->email = $email;
+      $administrative->content = $content;
+      $administrative->firm = $path;
+      $administrative->type = 'administrative';
+
+      if ($administrative->save()) {
+        DB::commit();
+        return SuccessNotification::get_notifications('success', __('Successfully Created Record'), 1500, 'completed');
+      }
+    } catch (\Throwable $th) {
+      DB::rollBack();
+      return ErrorNotification::get_notifications('error', __('An error has occurred'), 1500, 'completed');
+    }
+  }
+
+  static function editAdmission(NotificationForm $form, int $id)
+  {
+    $path = null;
+
+    $register = ConsultingServices::get_exists('notifications', ['id' => $id]);
+    if (!$register) {
+      return  InfoNotification::get_notifications('info', __('Record Not Found'), 1500, 'success');
+    }
+
+    $information = Notification::find($id);
+
+    DB::beginTransaction();
+    try {
+      if ($form->firm) {
+        $path = UploadService::upload('Firmas', $form->firm);
+      }
+
+      $email = SanitizeService::sanitize($form->email, 'email');
+      $content = SanitizeService::sanitize($form->content, 'html');
+
+      $information->email = $email;
+      $information->content = $content;
+      $information->firm = ($path) ? $path : $information->firm;
+      if ($information->save()) {
+        DB::commit();
+        return SuccessNotification::get_notifications('success', __('Successfully Updated Record'), 1500, 'completed');
+      }
+    } catch (\Throwable $th) {
+      DB::rollBack();
+      return ErrorNotification::get_notifications('error', __('An error has occurred'), 1500, 'completed');
+    }
+  }
+
+  static function destroyAdmission(int $id)
+  {
+    $exists = ConsultingServices::get_exists('notifications', ['id' => $id]);
+    if (!$exists) {
+      return  InfoNotification::get_notifications('info', __('Record Not Found'), 1500, 'success');
+    }
+    if (Notification::destroy($id)) {
+      return SuccessNotification::get_notifications('success', __('Successfully Deleted Record'), 1500, 'completed');
+    }
+  }
+
+  static function getAdmissions()
+  {
+    $registers = Notification::where('type', 'admission')->paginate(10);
+    return $registers;
+  }
+
+  static function getInformation(int $id)
+  {
+    return Notification::find($id);
+  }
+}
